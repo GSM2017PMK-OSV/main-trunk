4229563ec76b445ddad59388d034796d4b53be32
Сергей <svo1975pnv1982@gmail.com>
2025-08-29 21:15:41 +0300

    Create HybridCodeAnalyzer.py

---
+++ HybridCodeAnalyzer.py
@@ -0,0 +1,21 @@
+class HybridCodeAnalyzer:
+    def __init__(self):
+        self.models = {
+            'codebert': CodeBertModel(),
+            'graph_neural_net': GraphNN(),
+            'transformer': CodeTransformer(),
+            'geometric_dl': GeometricDeepLearningModel()
+        }
+        
+        self.ensemble_weights = self._train_ensemble_weights()
+    
+    async def analyze(self, code: str) -> Dict:
+        """Ансамблирование предсказаний multiple моделей"""
+        predictions = {}
+        
+        for name, model in self.models.items():
+            predictions[name] = await model.predict(code)
+        
+        # Взвешенное ансамблирование
+        final_prediction = self._ensemble_predictions(predictions)
+        return final_prediction
