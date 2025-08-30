e486e8d880ebe7934cb4fcf7c85fcac7c1631054
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 12:07:51 +0300

    Create NeuralFixPredictor.py

---
+++ NeuralFixPredictor.py
@@ -0,0 +1,15 @@
+class NeuralFixPredictor:
+    def __init__(self):
+        self.model = load_model('codefix-predictor')
+        self.tokenizer = CodeTokenizer()
+        
+    def predict_fix(self, error: Error, context: str) -> FixPrediction:
+        """Предсказывает исправление с помощью ИИ"""
+        tokens = self.tokenizer.tokenize(error, context)
+        prediction = self.model.predict(tokens)
+        
+        return {
+            'confidence': prediction.confidence,
+            'suggested_fix': prediction.fix,
+            'alternative_fixes': prediction.alternatives
+        }
